ce119ce512df0ec847885ae37e81823777be60d7
dale.band <dale.band@kakaocorp.com>
2022-11-08 12:25:21 +0900

    텍스트 컴포저블로 간단한 앱 빌드 완료 - BirthdayGreetingWithText

--- Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -38,7 +38,13 @@ import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            HappyBirthdayTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    BirthdayGreetingWithText(message = "Happy Birthday Sam!", from = "- from Emma")
+                }
+            }
+        }
     }
 }
 
@@ -46,15 +52,23 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BirthdayGreetingWithText(message: String, from: String) {
     // Create a column so that texts don't overlap
-    Column { }
+    Column {
+        Text(text = message, fontSize = 36.sp)
+        Text(text = from, fontSize = 24.sp)
+    }
 }
 
 // 5. Box 레이아웃 추
 @Composable
-fun BirthdayGreetingWithImage(message: String, from: String) { }
+fun BirthdayGreetingWithImage(message: String, from: String) {
+}
 
 // 4. 이미지 컴포저블 추가
 @Preview(showBackground = false)
 @Composable
-private fun BirthdayCardPreview() { }
+private fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        BirthdayGreetingWithText(message = "Happy Birthday Sam!", from = "- from Emma")
+    }
+}
 
