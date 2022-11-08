66c50b76cfb55829a87975dc5dce0da639834c08
Charlie_Moon <moon3482@cau.ac.kr>
2022-11-08 22:59:41 +0900

    메인액티비티 컴포즈 코드 추가

--- Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -16,10 +16,10 @@
 package com.example.happybirthday
 
 import android.os.Bundle
-import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -27,6 +27,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -38,7 +39,16 @@ import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            HappyBirthdayTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    BirthdayGreetingWithImage(
+                        stringResource(id = R.string.happy_birthday_text),
+                        String.format(stringResource(R.string.signature_text), "Charlie")
+                    )
+                }
+            }
+        }
     }
 }
 
@@ -46,15 +56,55 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BirthdayGreetingWithText(message: String, from: String) {
     // Create a column so that texts don't overlap
-    Column { }
+    Column {
+        Text(
+            text = message,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .background(color = Color.Green)
+                .fillMaxWidth()
+                .wrapContentWidth(
+                    Alignment.CenterHorizontally
+                )
+                .padding(start = 16.dp, top = 16.dp)
+        )
+        Text(
+            text = from, fontSize = 24.sp, modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color.Green)
+                .wrapContentWidth(
+                    Alignment.CenterHorizontally
+                )
+                .padding(start = 16.dp, end = 16.dp)
+        )
+    }
 }
+
 
 // 5. Box 레이아웃 추
 @Composable
-fun BirthdayGreetingWithImage(message: String, from: String) { }
+fun BirthdayGreetingWithImage(message: String, from: String) {
+    val image = painterResource(id = R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(message = message, from = from)
+    }
+}
 
 // 4. 이미지 컴포저블 추가
-@Preview(showBackground = false)
+@Preview(showBackground = true)
 @Composable
-private fun BirthdayCardPreview() { }
+private fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        BirthdayGreetingWithImage(stringResource(id = R.string.happy_birthday_text),
+            String.format(stringResource(R.string.signature_text), "Charlie"))
+    }
+}
 
