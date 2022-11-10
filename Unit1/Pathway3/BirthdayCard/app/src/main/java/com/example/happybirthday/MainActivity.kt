f40dbe85fb9a42ee41d9b89279fcfe2a1bf6af00
YuGyeong98 <kuk3097@naver.com>
2022-11-11 00:04:46 +0900

    refactor: 텍스트 컴포저블 가운데 배치

--- Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -59,16 +59,16 @@ fun BirthdayGreetingWithText(message: String, from: String) {
             fontSize = 36.sp,
             modifier = Modifier
                 .fillMaxWidth()
-                .wrapContentWidth(Alignment.Start)
-                .padding(start = 16.dp, top = 16.dp)
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(top = 16.dp)
         )
         Text(
             text = from,
             fontSize = 24.sp,
             modifier = Modifier
                 .fillMaxWidth()
-                .wrapContentWidth(Alignment.End)
-                .padding(start = 16.dp, end = 16.dp)
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(end = 16.dp)
         )
     }
 }
