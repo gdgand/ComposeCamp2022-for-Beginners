f9215f853b2eb3798ae0b8786c1aef30f64821fa
msjjang <dewlain@gmail.com>
2022-11-11 17:21:34 +0900

    commit body

--- Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/ui/theme/Theme.kt
+++ Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/ui/theme/Theme.kt
@@ -2,6 +2,7 @@ package com.example.composearticle.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
@@ -44,4 +45,5 @@ fun ComposeArticleTheme(
         shapes = Shapes,
         content = content
     )
+
 }
--- Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/ui/theme/Type.kt
+++ Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/ui/theme/Type.kt
@@ -1,5 +1,6 @@
 package com.example.composearticle.ui.theme
 
+
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
