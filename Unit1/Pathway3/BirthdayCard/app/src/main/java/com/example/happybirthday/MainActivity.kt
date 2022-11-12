25391a0be0f7c6bdebc2c2aac516f6083363eb66
hojooneum <bldolphin96@gmail.com>
2022-11-12 17:44:07 +0900

    add GreetingText

--- Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -16,29 +16,24 @@
 package com.example.happybirthday
 
 import android.os.Bundle
-import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            HappyBirthdayTheme() {
+                GreetingCard(name = "HoJoon", cardColor = MaterialTheme.colors.background)
+            }
+        }
     }
 }
 
@@ -54,7 +49,22 @@ fun BirthdayGreetingWithText(message: String, from: String) {
 fun BirthdayGreetingWithImage(message: String, from: String) { }
 
 // 4. 이미지 컴포저블 추가
-@Preview(showBackground = false)
+@Preview(name = "BirthdayCard-Preview", showBackground = false)
 @Composable
-private fun BirthdayCardPreview() { }
+private fun BirthdayCardPreview() {
+    HappyBirthdayTheme() {
 
+    }
+}
+
+@Composable
+fun GreetingCard(name: String, cardColor: androidx.compose.ui.graphics.Color) {
+    return Surface(color = cardColor) {
+        GreetingText(name = name)
+    }
+}
+
+@Composable
+fun GreetingText(name: String) {
+    return Text(text = name)
+}
