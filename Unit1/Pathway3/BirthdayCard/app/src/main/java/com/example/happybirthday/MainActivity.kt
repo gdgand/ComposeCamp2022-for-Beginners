e48c407d41e26f3c5b84b2de233f563e2f560a31
Sungjun Moon <project_m_@hotmail.com>
2022-11-12 23:24:02 +0900

    unit1_pathway3_add_images_to_your_android_app

--- Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -42,7 +42,8 @@ class MainActivity : ComponentActivity() {
             HappyBirthdayTheme {
                 // A surface container that uses the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    BirthdayGreetingWithText( "Happy Birthday Sam!", "- from Emma")
+                    BirthdayGreetingWithImage( stringResource(R.string.happy_birthday_text),
+                        stringResource(R.string.signature_text))
                 }
             }
         }
@@ -54,27 +55,52 @@ class MainActivity : ComponentActivity() {
 fun BirthdayGreetingWithText(message: String, from: String) {
     // Create a column so that texts don't overlap
     Column {
+        // Add two text, let them appear on top of each other
         Text(
             text = message,
             fontSize = 36.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, top = 16.dp)
+
         )
         Text(
             text = from,
             fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, end = 16.dp)
         )
     }
 }
 
 // 5. Box 레이아웃 추
 @Composable
-fun BirthdayGreetingWithImage(message: String, from: String) { }
+fun BirthdayGreetingWithImage(message: String, from: String) {
+    val image = painterResource(R.drawable.androidparty)
+    // Create a box to overlap image and texts
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(message = message, from = from)
+    }
+}
 
 // 4. 이미지 컴포저블 추가
 @Preview(showBackground = false)
 @Composable
 private fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        BirthdayGreetingWithText( "Happy Birthday Sam!", "- from Emma")
+        BirthdayGreetingWithImage( stringResource(R.string.happy_birthday_text),
+            stringResource(R.string.signature_text))
     }
 }
 
