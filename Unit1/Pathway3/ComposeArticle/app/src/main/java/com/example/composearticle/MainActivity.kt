3b8f0f67ef9f08af5e317eada3d944d94859cddc
wkdqhrud4870 <jangbg4870@gmail.com>
2022-11-12 19:26:38 +0900

    학습_COMPOSE 사용 시 알아야 하는 ANDROID 기본사항_FIRST ANDROID APP_BASIC LAYOUT_연습: Compose 기본사항_2. Compose 도움말

--- Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -24,12 +22,23 @@ import com.example.composearticle.ui.theme.ComposeArticleTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            ComposeArticleApp()
+        }
     }
 }
 
 @Composable
-fun ComposeArticleApp() { }
+fun ComposeArticleApp() {
+    ComposeArticleTheme() {
+        Surface(color = MaterialTheme.colors.background){
+            ArticleCard(title = stringResource(id = R.string.title_jetpack_compose_tutorial),
+                shortDescription = stringResource(id = R.string.compose_short_desc),
+                longDescription = stringResource(id = R.string.compose_long_desc),
+                imagePainter = painterResource(id = R.drawable.bg_compose_background))
+        }
+    }
+}
 
 @Composable
 private fun ArticleCard(
@@ -39,10 +48,33 @@ private fun ArticleCard(
     imagePainter: Painter,
     modifier: Modifier = Modifier,
 ) {
-    Column() { }
+    Column() {
+        Image(painter = imagePainter,
+            contentDescription = null
+        )
+        Column (
+            modifier = modifier.padding(20.dp)
+        ){
+            Text(
+                text = title,
+                fontSize = 30.sp
+            )
+            Text(
+                text = shortDescription,
+                modifier = modifier.padding(top = 10.dp, bottom = 10.dp)
+            )
+            Text(
+                text = longDescription,
+                modifier = modifier.padding(top = 10.dp, bottom = 10.dp)
+            )
+        }
+
+    }
 }
 
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() { }
+fun DefaultPreview() {
+    ComposeArticleApp()
+}
