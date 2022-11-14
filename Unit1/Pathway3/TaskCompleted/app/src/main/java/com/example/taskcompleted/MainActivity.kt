27c4cf6410bfd9559fe095587075a2671b36713e
hwani0211 <hwani0211@gmail.com>
2022-11-14 19:32:00 +0900

    Unit1_Pathway3_TaskCompleted까지 구현 완료

--- Unit1/Pathway3/TaskCompleted/app/src/main/java/com/example/taskcompleted/MainActivity.kt
+++ Unit1/Pathway3/TaskCompleted/app/src/main/java/com/example/taskcompleted/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.taskcompleted
 
 import android.os.Bundle
+import android.util.Printer
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -10,6 +11,8 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -22,15 +25,53 @@ import com.example.taskcompleted.ui.theme.TaskCompletedTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent { 
+            TaskCompletedTheme() {
+                TaskCompletedScreen(text1 = "All tasks completed", text2 = "Nice work!")
+            }
+        }
     }
 }
 
 @Composable
-fun TaskCompletedScreen() {
-    Column( ) { }
+fun TaskCompletedScreen(
+    text1: String,
+    text2: String,
+//    modifier: Modifier
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_task_completed),
+            contentDescription = null
+        )
+        Text(
+            text = text1,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = text2,
+            fontSize = 16.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() { }
+fun DefaultPreview() {
+    TaskCompletedTheme {
+        TaskCompletedScreen(
+            text1 = "All tasks completed",
+            text2 = "Nice work!",
+//            modifier = Modifier
+//                .padding(top = 24.dp, bottom = 8.dp)
+        )
+    }
+}
