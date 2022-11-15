ed1943b86f7334438691f31c792916d2fd591f3f
soopeach <hsjeon584@gmail.com>
2022-11-16 01:59:02 +0900

    :white_check_mark: Task Manager 완료

--- Unit1/Pathway3/TaskCompleted/app/src/main/java/com/example/taskcompleted/MainActivity.kt
+++ Unit1/Pathway3/TaskCompleted/app/src/main/java/com/example/taskcompleted/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -20,7 +21,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TaskCompletedTheme() {
-                ComposeHelper()
+//                ComposeHelper()
+                TaskCompletedScreen()
             }
         }
     }
@@ -28,6 +30,35 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TaskCompletedScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        StateIcon()
+        Text(
+            text = stringResource(R.string.text_task_state),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = stringResource(R.string.text_notification),
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Composable
+fun StateIcon() {
+    val image = painterResource(id = R.drawable.ic_task_completed)
+
+    Image(
+        painter =
+        image,
+        contentDescription = "taskStateImage"
+    )
 }
 
 @Composable
