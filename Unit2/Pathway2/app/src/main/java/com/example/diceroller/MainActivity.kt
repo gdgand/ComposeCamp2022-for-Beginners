b3e18d5b14103b0b25ffcd462f907c6a6591e1d7
msjjang <dewlain@gmail.com>
2022-11-15 16:37:35 +0900

    import dice images

--- Unit2/Pathway2/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Unit2/Pathway2/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,15 +3,19 @@ package com.example.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,15 +29,25 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview
 @Composable
 fun DiceRollerApp() {
-    DiceWithButtonAndImage(modifier = Modifier.fillMaxSize()
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
         .wrapContentSize(Alignment.Center))
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-
+    Column(
+        modifier = Modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(id = R.drawable.dice_1),
+            contentDescription = "1")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { /*TODO*/ }) {
+            Text(stringResource(R.string.roll))
+        }
+    }
 }
