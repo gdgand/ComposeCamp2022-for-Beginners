b617d6e2858d7a96f2d58475249375e9b33a92f1
soopeach <hsjeon584@gmail.com>
2022-11-17 01:04:55 +0900

    :white_check_mark: Compose 사분면 완료

--- Unit1/Pathway3/TaskCompleted/app/src/main/java/com/example/taskcompleted/MainActivity.kt
+++ Unit1/Pathway3/TaskCompleted/app/src/main/java/com/example/taskcompleted/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,9 +33,71 @@ class MainActivity : ComponentActivity() {
         setContent {
             TaskCompletedTheme() {
 //                ComposeHelper()
-                TaskCompletedScreen()
+//                TaskCompletedScreen()
+                ComposeQuadrant()
             }
         }
+    }
+}
+
+@Composable
+fun ComposeQuadrant() {
+    Row(Modifier.fillMaxHeight()) {
+        Column(modifier = Modifier.weight(1f)) {
+            ComposeCard(
+                "Text composable",
+                "Displays text and follows Material Design guidelines.",
+                Color.Green,
+                modifier = Modifier.weight(1f)
+            )
+            ComposeCard(
+                "Row composable",
+                "A layout composable that places its children in a horizontal sequence.",
+                Color.Cyan,
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Column(modifier = Modifier.weight(1f)) {
+            ComposeCard(
+                "Image composable",
+                "Creates a composable that lays out and draws a given Painter class object.",
+                Color.Yellow,
+                modifier = Modifier.weight(1f)
+            )
+            ComposeCard(
+                "Column composable",
+                "A layout composable that places its children in a vertical sequence.",
+                Color.LightGray,
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+fun ComposeCard(
+    title: String,
+    content: String,
+    backgroundColor: Color,
+    modifier: Modifier
+) {
+    Column(
+        modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = content,
+            textAlign = TextAlign.Justify
+        )
     }
 }
 
