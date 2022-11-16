03e893ddf0e8a36aa0f788c68580578465d2afba
gidskql6671 <gidskql6671@gmail.com>
2022-11-16 11:14:10 +0900

    연락처 부분 작업 중

--- Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -6,9 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Email
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -35,12 +38,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ComposeArticleApp() {
-    Column(modifier = Modifier.background(colorResource(R.color.background))) {
+    Column(modifier = Modifier
+        .background(colorResource(R.color.background))
+        .fillMaxSize()
+        .padding(16.dp)) {
+
         ArticleCard(
             title = stringResource(R.string.name),
             shortDescription = stringResource(R.string.compose_short_desc),
             longDescription = stringResource(R.string.compose_long_desc),
-            imagePainter = painterResource(R.drawable.android_logo))
+            imagePainter = painterResource(R.drawable.android_logo),
+            modifier = Modifier.weight(4f)
+        )
+
+        Contact(modifier = Modifier.weight(1f))
     }
 }
 
@@ -50,14 +61,13 @@ private fun ArticleCard(
     shortDescription: String,
     longDescription: String,
     imagePainter: Painter,
-    modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp),
+        modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center) {
+
         Image(painter = imagePainter,
             contentDescription = null,
             modifier = Modifier
@@ -66,6 +76,25 @@ private fun ArticleCard(
         )
         Text(text = title, color = Color.White)
         Text(text = shortDescription, color = colorResource(R.color.content))
+    }
+}
+
+
+@Composable
+private fun Contact(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        ContactCard(content = "test1")
+        ContactCard(content = "test2")
+        ContactCard(content = "test3")
+        ContactCard(content = "test4")
+    }
+}
+
+@Composable
+private fun ContactCard(content: String) {
+    Row(horizontalArrangement = Arrangement.SpaceBetween) {
+        Icon(imageVector = Icons.Rounded.Email, contentDescription = null)
+        Text(text = content, color = colorResource(R.color.white))
     }
 }
 
