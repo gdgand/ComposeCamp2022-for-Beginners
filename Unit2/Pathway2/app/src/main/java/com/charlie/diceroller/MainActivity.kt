cbddae8485954965727562ee194a92c55cc507e6
Charlie_Moon <moon3482@cau.ac.kr>
2022-11-17 22:07:29 +0900

    수정자 수정

--- Unit2/Pathway2/app/src/main/java/com/charlie/diceroller/MainActivity.kt
+++ Unit2/Pathway2/app/src/main/java/com/charlie/diceroller/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.charlie.diceroller.ui.theme.DiceRollerTheme
@@ -25,12 +27,14 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
 
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DiceRollerApp(){
-    DiceWithButtonAndImage()
+fun DiceRollerApp() {
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center))
 }
