e225cca0c18cc5a5be3f60248fa1a756a4f6100a
ZungTa <quddnr0708@gmail.com>
2022-11-18 17:33:10 +0900

    Unit2 Art Space UI 개발

--- Unit2/Pathway3/ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ Unit2/Pathway3/ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    ArtSpaceApp("Android")
+                    ArtSpaceApp()
                 }
             }
         }
@@ -30,14 +34,74 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ArtSpaceApp(name: String) {
-    Text(text = "Hello $name!")
+fun ArtSpaceApp() {
+    val modifier = Modifier;
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Box {
+            Box(
+                modifier = modifier.border(2.dp, Color.Black)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.img1),
+                    contentDescription = null,
+                    modifier = modifier.padding(20.dp)
+                )
+            }
+        }
+
+        Card(
+            elevation = 5.dp,
+            modifier = modifier.padding(top = 20.dp)
+        ) {
+            Column(
+                modifier = modifier.padding(10.dp)
+            ) {
+                Text(
+                    text = "Title"
+                )
+                Text(
+                    text = "Sub Title (2021)"
+                )
+            }
+        }
+
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp, vertical = 40.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            val buttonWidth = 130.dp
+
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = modifier.width(buttonWidth)
+            ) {
+                Text(text = "Previous")
+            }
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = modifier.width(buttonWidth)
+            ) {
+                Text(text = "Next")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ArtSpaceTheme {
-        ArtSpaceApp("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            ArtSpaceApp()
+        }
     }
 }
