944f2f2d40a6b9f19c930e0d4513ef622453b23f
gidskql6671 <gidskql6671@gmail.com>
2022-11-19 21:09:27 +0900

    tip 비율 텍스트필드 추가

--- Unit2/Pathway3/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Unit2/Pathway3/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -42,9 +42,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TipTimeScreen() {
     var amountInput by remember { mutableStateOf("") }
+    var tipInput by remember { mutableStateOf("") }
 
     val amount = amountInput.toDoubleOrNull() ?: 0.0
-    val tip = calculateTip(amount)
+    val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount, tipPercent)
 
     Column(
         modifier = Modifier.padding(32.dp),
@@ -60,6 +62,11 @@ fun TipTimeScreen() {
             label = R.string.bill_amount,
             value = amountInput,
             onValueChange = { amountInput = it })
+        EditNumberField(
+            label = R.string.how_was_the_service,
+            value = tipInput,
+            onValueChange = { tipInput = it }
+        )
         Spacer(Modifier.height(24.dp))
         Text(
             text = stringResource(R.string.tip_amount, tip),
