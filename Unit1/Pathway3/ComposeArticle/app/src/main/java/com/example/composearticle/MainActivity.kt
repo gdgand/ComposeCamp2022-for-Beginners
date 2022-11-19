975646858ae92b43a38c133f945b0e264f9f5cfc
ShinJaehun <gunsmoke.shin@gmail.com>
2022-11-19 13:01:50 +0900

    finish compose_article

--- Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -4,15 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -29,7 +28,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ComposeArticleApp() { }
+fun ComposeArticleApp() {
+    val image = painterResource(id = R.drawable.bg_compose_background)
+    ArticleCard(
+        title = stringResource(R.string.title),
+        shortDescription = stringResource(R.string.short_description),
+        longDescription = stringResource(id = R.string.long_description),
+        imagePainter = image)
+}
 
 @Composable
 private fun ArticleCard(
@@ -39,10 +45,38 @@ private fun ArticleCard(
     imagePainter: Painter,
     modifier: Modifier = Modifier,
 ) {
-    Column() { }
+    Column() {
+        Image (
+            painter = imagePainter,
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth(),
+        )
+        Text (
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp, top = 16.dp, bottom = 16.dp)
+        )
+        Text (
+            text = shortDescription,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text (
+            text = longDescription,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp, top = 16.dp, bottom = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+
+
+    }
 }
 
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() { }
+fun DefaultPreview() {
+    ComposeArticleApp()
+}
