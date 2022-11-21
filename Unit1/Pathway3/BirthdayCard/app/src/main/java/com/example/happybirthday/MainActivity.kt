36f5cc81835a96560f709887ce9acafebbd329b2
JuHyeokLee <bjuhyeok@gmail.com>
2022-11-21 21:49:56 +0900

    image composable and modifier

--- Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -41,7 +41,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HappyBirthdayTheme() {
                 Surface(color= MaterialTheme.colors.background) {
-                    com.example.happybirthday.BirthdayGreetingWithText("HappyBirthday", "from Me")
+                    com.example.happybirthday.BirthdayGreetingWithImage(getString(R.string.happy_birthday_text), "from Me")
                 }
             }
         }
@@ -55,26 +55,46 @@ fun BirthdayGreetingWithText(message: String, from: String) {
     Column {
         Text(
             text=message,
-            fontSize=36.sp
+            fontSize=36.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, top = 16.dp)
         )
 
         Text(
             text=from,
-            fontSize=24.sp
+            fontSize=24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, end = 16.dp)
         )
     }
 }
 
 // 5. Box 레이아웃 추
 @Composable
-fun BirthdayGreetingWithImage(message: String, from: String) { }
+fun BirthdayGreetingWithImage(message: String, from: String) {
+    val image = painterResource(R.drawable.androidparty)
+    Box{
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier.fillMaxHeight().fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(message = message, from = from )
+    }
+}
 
 // 4. 이미지 컴포저블 추가
+
 @Preview(showBackground = false)
 @Composable
 private fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        BirthdayGreetingWithText("Happy Birthday", "from Me")
+        BirthdayGreetingWithImage("Happy Birthday", "- from Me")
     }
 }
 
