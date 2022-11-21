b392d7f4c88f598006c8ea0a972da65f1badb2b4
qwqeqrqwqeqr <qwqeqrqwqeqr@github.com>
2022-11-21 15:35:53 +0900

    [Update] Unit1 TaskCompleted 이미지 텍스트 리소스를 추가합니다. 레이아웃을 수정합니다.

--- Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -9,13 +9,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextAlign.Companion.Justify
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -25,7 +23,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeArticleTheme{
+            ComposeArticleTheme {
                 Surface(color = MaterialTheme.colors.background) {
                     ArticleCard(
                         title = stringResource(R.string.title_jetpack_compose_tutorial),
@@ -34,40 +32,58 @@ class MainActivity : ComponentActivity() {
                         imagePainter = painterResource(R.drawable.bg_compose_background)
                     )
 
+
                 }
             }
         }
     }
-}
 
-@Composable
-fun ComposeArticleApp() { }
-
-@Composable
-private fun ArticleCard(
-    title: String,
-    shortDescription: String,
-    longDescription: String,
-    imagePainter: Painter,
-    modifier: Modifier = Modifier,
-) {
-    Column() {
-        Image(painter = imagePainter, contentDescription = null,modifier.fillMaxWidth())
-        Text(text = title, fontSize = 24.sp, modifier = modifier.fillMaxWidth().padding(16.dp))
-        Text(text = shortDescription, textAlign = TextAlign.Justify,modifier = modifier.fillMaxWidth().padding(start = 16.dp, end = 16.dp))
-        Text(text = longDescription, textAlign = TextAlign.Justify,modifier = modifier.fillMaxWidth().padding(16.dp))
+    @Composable
+    fun ComposeArticleApp() {
     }
-}
+
+    @Composable
+    private fun ArticleCard(
+        title: String,
+        shortDescription: String,
+        longDescription: String,
+        imagePainter: Painter,
+        modifier: Modifier = Modifier,
+    ) {
+        Column() {
+            Image(painter = imagePainter, contentDescription = null, modifier.fillMaxWidth())
+            Text(
+                text = title, fontSize = 24.sp, modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+            Text(
+                text = shortDescription,
+                textAlign = TextAlign.Justify,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp)
+            )
+            Text(
+                text = longDescription,
+                textAlign = TextAlign.Justify,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+        }
+    }
 
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ArticleCard(
-        title = stringResource(R.string.title_jetpack_compose_tutorial),
-        shortDescription = stringResource(R.string.compose_short_desc),
-        longDescription = stringResource(id = R.string.compose_long_desc),
-        imagePainter = painterResource(R.drawable.bg_compose_background)
-    )
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        ArticleCard(
+            title = stringResource(R.string.title_jetpack_compose_tutorial),
+            shortDescription = stringResource(R.string.compose_short_desc),
+            longDescription = stringResource(id = R.string.compose_long_desc),
+            imagePainter = painterResource(R.drawable.bg_compose_background)
+        )
 
+    }
 }
