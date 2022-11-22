d1d4984ef4cb706027f8c63ff93e394cb8bc2082
Charlie_Moon <moon3482@cau.ac.kr>
2022-11-22 21:09:35 +0900

    라벨 추가 및 텍스트 필드 가로 사이즈 수정 싱글라인 추가 키보드 옵션 추가

--- Unit2/Pathway3/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Unit2/Pathway3/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
@@ -11,6 +12,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -52,7 +54,20 @@ fun EditNumberField() {
     var amountInput by remember {
         mutableStateOf("")
     }
-    TextField(value = amountInput, onValueChange = {amountInput = it})
+    TextField(
+        value = amountInput,
+        onValueChange = { amountInput = it },
+        label = {
+            Text(
+                text = stringResource(
+                    id = R.string.cost_of_service
+                )
+            )
+        },
+        modifier = Modifier.fillMaxWidth(),
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
 }
 
 @Preview(showBackground = true)
