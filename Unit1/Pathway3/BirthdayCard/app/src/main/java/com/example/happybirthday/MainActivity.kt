c7991a8b45c6d26519ff3e48bc660e1bf22cd50f
eunjeong Lee <tbig1019@gmail.com>
2022-11-22 21:48:12 +0900

    Android 앱에 이미지 추가

--- Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -19,13 +19,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -34,7 +39,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Surface(color = MaterialTheme.colors.background) {
-                BirthdayGreetingWithText(message = "HappyBirthDay ^^", from = "- from Eunjeong" )
+                BirthdayGreetingWithImage(
+                    message = stringResource(R.string.happy_birthday_text), from = stringResource(
+                        R.string.signature_text
+                    )
+                )
             }
         }
     }
@@ -47,11 +56,19 @@ fun BirthdayGreetingWithText(message: String, from: String) {
     Column {
         Text(
             text = message,
-            fontSize = 36.sp
+            fontSize = 36.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, top = 16.dp)
         )
         Text(
             text = from,
-            fontSize = 24.sp
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, end = 16.dp)
         )
     }
 }
@@ -59,6 +76,18 @@ fun BirthdayGreetingWithText(message: String, from: String) {
 // 5. Box 레이아웃 추
 @Composable
 fun BirthdayGreetingWithImage(message: String, from: String) {
+    val image = painterResource(R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        BirthdayGreetingWithText(message = message, from = from)
+    }
 
 }
 
@@ -72,7 +101,7 @@ fun Greeting(name: String) {
 @Composable
 private fun BirthdayCardPreview() {
     HappyBirthdayTheme() {
-        BirthdayGreetingWithText("HappyBirthDay~!!", "- from Eunjeong :)")
+        BirthdayGreetingWithImage("HappyBirthDay~!!", "- from Eunjeong :)")
     }
 }
 
