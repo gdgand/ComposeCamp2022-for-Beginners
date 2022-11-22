e572fdf366bb860b0240ad97bb694dcaa1fe10c3
ZungTa <quddnr0708@gmail.com>
2022-11-23 00:15:43 +0900

    Unit4 unscramble

--- Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/android/unscramble/data/WordsData.kt
+++ Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/android/unscramble/data/WordsData.kt
@@ -19,6 +19,7 @@ package com.example.android.unscramble.data
 const val MAX_NO_OF_WORDS = 10
 const val SCORE_INCREASE = 20
 
+
 // Set with all the words for the Game
 val allWords: Set<String> =
     setOf(
