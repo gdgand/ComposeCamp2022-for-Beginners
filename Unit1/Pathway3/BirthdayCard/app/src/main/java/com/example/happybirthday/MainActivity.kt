f0124dcc414e8fcc539a16b6ecfb14008e0cc163
hojooneum <bldolphin96@gmail.com>
2022-11-24 00:30:17 +0900

    Complete Birthday Card

--- Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -18,12 +18,17 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -33,50 +38,59 @@ class MainActivity : ComponentActivity() {
         setContent {
             HappyBirthdayTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    BirthdayGreetingWithText(message = "Happy Birthday Hojoon!", from = "- from Kim")
+                    BirthdayGreetingWithImage(
+                        message = getString(R.string.happy_birthday_text),
+                        from = getString(R.string.signature_text)
+                    )
                 }
             }
         }
     }
 }
 
-// 7. 텍스트 정렬 및 패딩 추가
 @Composable
 fun BirthdayGreetingWithText(message: String, from: String) {
-    // Create a column so that texts don't overlap
     Column {
-        Text(
-            text = message,
-            fontSize = 36.sp,
-        )
-        Text(
-            text = from,
-            fontSize = 24.sp
-        )
+    Text(
+        text = message,
+        fontSize = 36.sp,
+        modifier = androidx.compose.ui.Modifier
+            .fillMaxWidth()
+            .wrapContentWidth(Alignment.CenterHorizontally)
+            .padding(start = 16.dp, top = 16.dp)
+    )
+    Text(
+        text = from,
+        fontSize = 24.sp,
+        modifier = androidx.compose.ui.Modifier
+            .fillMaxWidth()
+            .wrapContentWidth(Alignment.CenterHorizontally)
+            .padding(start = 16.dp, end = 26.dp)
+    )
     }
 }
 
-// 5. Box 레이아웃 추가
 @Composable
-fun BirthdayGreetingWithImage(message: String, from: String) { }
+fun BirthdayGreetingWithImage(message: String, from: String) {
+    val image = painterResource(id = R.drawable.androidparty)
+    Column {
+        Box {
+            Image(
+                painter = image,
+                contentDescription = null,
+                modifier = androidx.compose.ui.Modifier.fillMaxHeight().fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+            BirthdayGreetingWithText(message = message, from = from)
+        }
 
-// 4. 이미지 컴포저블 추가
+    }
+}
+
 @Preview(name = "BirthdayCard-Preview", showBackground = false)
 @Composable
 private fun BirthdayCardPreview() {
     HappyBirthdayTheme() {
-        BirthdayGreetingWithText(message = "Happy Birthday HoJoon", from = "- from Kim")
+        BirthdayGreetingWithImage(message = "Happy Birthday Dan!", from = "- from Kim")
     }
-}
-
-@Composable
-fun GreetingCard(name: String, cardColor: androidx.compose.ui.graphics.Color) {
-    return Surface(color = cardColor) {
-        GreetingText(name = name)
-    }
-}
-
-@Composable
-fun GreetingText(name: String) {
-    return Text(text = name)
 }
--- Unit1/Pathway3/NameCard/app/src/main/java/com/example/namecard/MainActivity.kt
+++ Unit1/Pathway3/NameCard/app/src/main/java/com/example/namecard/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.namecard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.namecard.ui.theme.NameCardTheme
 
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    NameCard()
                 }
             }
         }
@@ -40,4 +44,28 @@ fun DefaultPreview() {
     NameCardTheme {
         Greeting("Android")
     }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun NameCard() {
+    ProfileBlock()
+    ContentBlock()
+}
+
+@Preview(showBackground = false)
+@Composable
+fun ProfileBlock(imgSrc: String = "./android_logo.png", fullName: String = "John Doe", title: String = "title") {
+    val profileImg = painterResource(id = R.drawable.android_logo)
+    Column() {
+        Row() {
+            Image(painter = profileImg, contentDescription = "profile image")
+        }
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun ContentBlock() {
+    
 }
