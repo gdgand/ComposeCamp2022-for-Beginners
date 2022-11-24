008f2ad65e35ec9ba88eb2aa87f2a2589bfcf5bd
tamama <qutepersona@gmail.com>
2022-11-25 08:57:35 +0900

    2022-11-25  WoofDone

--- Unit3/Pathway3/WoofCodelab/app/src/main/java/com/example/woofcodelab/MainActivity.kt
+++ Unit3/Pathway3/WoofCodelab/app/src/main/java/com/example/woofcodelab/MainActivity.kt
@@ -20,33 +20,36 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ComposeCompilerApi
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.woofcodelab.data.Dog
 import com.example.woofcodelab.data.dogs
 import com.example.woofcodelab.ui.theme.WoofTheme
+import kotlin.math.exp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -86,20 +89,79 @@ fun WoofApp() {
  */
 @Composable
 fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
+    var expanded by remember { mutableStateOf(false)}
     Card(
         modifier = modifier.padding(8.dp),
         elevation = 4.dp
     ) {
-        Row(
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
         ) {
-            DogIcon(dog.imageResourceId)
-            DogInformation(dog.name, dog.age)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                DogIcon(dog.imageResourceId)
+                DogInformation(dog.name, dog.age)
+                Spacer(Modifier.weight(1f))
+                DogItemButton(
+                    expanded = expanded
+                    , onClick = { expanded = !expanded })
+            }
+            if (expanded){
+                DogHobby(dog.hobbies)
+            }
+
         }
+        
     }
 }
+
+@Composable
+private fun DogItemButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+  IconButton(onClick = onClick) {
+        Icon(
+            imageVector = if(expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+            tint = MaterialTheme.colors.secondary,
+            contentDescription = stringResource(id = R.string.expand_button_content_description))
+  }
+}
+
+//@Composable
+//fun IconButton(
+//    onClick: () -> Unit,
+//    modifier: Modifier = Modifier,
+//    enabled: Boolean = true,
+//    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+//    content: @Composable () -> Unit
+//) {
+//    Box(
+//        modifier = modifier
+//            .minimumTouchTargetSize()
+//            .clickable(
+//                onClick = onClick,
+//                enabled = enabled,
+//                role = Role.Button,
+//                interactionSource = interactionSource,
+//                indication = rememberRipple(bounded = false, radius = RippleRadius)
+//            ),
+//        contentAlignment = Alignment.Center
+//    ) {
+//        val contentAlpha = if (enabled) LocalContentAlpha.current else ContentAlpha.disabled
+//        CompositionLocalProvider(LocalContentAlpha provides contentAlpha, content = content)
+//    }
+//}
 
 @Composable
 fun WoofTopAppBar(modifier: Modifier = Modifier){
@@ -111,7 +173,8 @@ fun WoofTopAppBar(modifier: Modifier = Modifier){
     ) {
         Image(
             modifier = Modifier
-                .size(64.dp).padding(8.dp),
+                .size(64.dp)
+                .padding(8.dp),
             painter = painterResource(id = R.drawable.ic_woof_logo)
             , contentDescription = null)
         Text(text = stringResource(id = R.string.app_name),
@@ -119,6 +182,26 @@ fun WoofTopAppBar(modifier: Modifier = Modifier){
     }
 }
 
+@Composable
+fun DogHobby(@StringRes dogHobby: Int, modifier: Modifier = Modifier){
+    Column (
+        modifier = modifier.padding(
+            start = 16.dp,
+            top = 8.dp,
+            bottom = 16.dp,
+            end = 16.dp
+        )
+    ) {
+        Text(
+            text = stringResource(id = R.string.about),
+            style = MaterialTheme.typography.h3
+        )
+        Text(
+            text = stringResource(id = dogHobby),
+            style = MaterialTheme.typography.body1
+        )
+    }
+}
 
 /**
  * Composable that displays a photo of a dog.
