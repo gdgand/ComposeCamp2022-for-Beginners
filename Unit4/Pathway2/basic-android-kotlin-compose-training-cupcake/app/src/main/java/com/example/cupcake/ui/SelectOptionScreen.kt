b16cade12411748d35d32354e2e437fb7a1972cc
hhp227 <hhp227@kortek.co.kr>
2022-11-24 11:27:58 +0900

    Cupcake App 완성

--- Unit4/Pathway2/basic-android-kotlin-compose-training-cupcake/app/src/main/java/com/example/cupcake/ui/SelectOptionScreen.kt
+++ Unit4/Pathway2/basic-android-kotlin-compose-training-cupcake/app/src/main/java/com/example/cupcake/ui/SelectOptionScreen.kt
@@ -50,13 +50,13 @@ fun SelectOptionScreen(
     subtotal: String,
     options: List<String>,
     onSelectionChanged: (String) -> Unit = {},
-    // TODO: add onCancelButtonClicked
-    // TODO: add onNextButtonClicked
+    onCancelButtonClicked: () -> Unit = {},
+    onNextButtonClicked: () -> Unit = {},
     modifier: Modifier = Modifier
 ){
     var selectedValue by rememberSaveable { mutableStateOf("") }
 
-    Column (modifier = modifier.padding(16.dp)){
+    Column(modifier = modifier.padding(16.dp)) {
         options.forEach { item ->
             Row (
                 modifier = Modifier.selectable(
@@ -85,18 +85,18 @@ fun SelectOptionScreen(
                 .align(Alignment.End)
                 .padding(top = 16.dp, bottom = 16.dp)
         )
-        Row (
+        Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.spacedBy(16.dp)
-        ){
-            OutlinedButton(modifier = Modifier.weight(1f), onClick = { /* TODO: handle cancel button */ }) {
+        ) {
+            OutlinedButton(modifier = Modifier.weight(1f), onClick = onCancelButtonClicked) {
                 Text(stringResource(R.string.cancel))
             }
             Button(
                 modifier = Modifier.weight(1f),
                 // the button is enabled when the user makes a selection
                 enabled = selectedValue.isNotEmpty(),
-                onClick = { /* TODO: handle next button */ }
+                onClick = onNextButtonClicked
             ) {
                 Text(stringResource(R.string.next))
             }
