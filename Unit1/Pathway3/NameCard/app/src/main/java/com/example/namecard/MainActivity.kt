3419fdd7523c91d022e5d755e1ebdca9259e3d43
hojooneum <bldolphin96@gmail.com>
2022-11-25 17:58:20 +0900

    complete NameCard

--- Unit1/Pathway3/NameCard/app/src/main/java/com/example/namecard/MainActivity.kt
+++ Unit1/Pathway3/NameCard/app/src/main/java/com/example/namecard/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.namecard
 
+import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.ColorRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.modifier.ModifierLocalConsumer
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.namecard.ui.theme.NameCardTheme
+import java.time.format.TextStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +39,6 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
                     NameCard()
                 }
@@ -33,39 +47,144 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NameCardTheme {
-        Greeting("Android")
-    }
-}
+/*@Composable
+fun MyTheme(content: @Composable () -> Unit) {
+    MaterialTheme(content = content)
+}*/
 
 @Preview(showBackground = false)
 @Composable
 fun NameCard() {
-    ProfileBlock()
-    ContentBlock()
-}
-
-@Preview(showBackground = false)
-@Composable
-fun ProfileBlock(imgSrc: String = "./android_logo.png", fullName: String = "John Doe", title: String = "title") {
-    val profileImg = painterResource(id = R.drawable.android_logo)
-    Column() {
-        Row() {
-            Image(painter = profileImg, contentDescription = "profile image")
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                .wrapContentHeight(Alignment.Top)
+                .background(color = Color(0xFF003A49)),
+        ) {
+            Column {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.weight(3f, true)
+                ) {
+                    ProfileBlock(
+                        imgSrc = R.drawable.android_logo,
+                        fullName = "Dan Brown",
+                        title = "Android Developer Extraordinaire"
+                    )
+                }
+                Box(
+                    contentAlignment = Alignment.TopStart,
+                    modifier = Modifier.weight(1f, true)) {
+                    ContentBlock()
+                }
+            }
         }
-    }
 }
 
-@Preview(showBackground = false)
+@Composable
+fun ProfileBlock(imgSrc: Int, fullName: String, title: String) {
+        val AndroidGreen = Color(0xFF48dc9a)
+        val profileImg = painterResource(id = imgSrc)
+        Column(
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Row {
+                Image(
+                    painter = profileImg,
+                    contentDescription = "profile image",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(100.dp)
+                )
+            }
+            Row {
+                Text(
+                    text = fullName,
+                    style = androidx.compose.ui.text.TextStyle(
+                        fontSize = 40.sp,
+                        color = Color.White,
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentWidth(Alignment.CenterHorizontally)
+                )
+            }
+            Row {
+                Text(
+                    text = title,
+                    style = androidx.compose.ui.text.TextStyle(
+                        fontSize = 10.sp,
+                        color = AndroidGreen,
+                        fontWeight = FontWeight(500),
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentWidth(Alignment.CenterHorizontally)
+                )
+            }
+        }
+}
+
 @Composable
 fun ContentBlock() {
-    
+        val AndroidGreen = Color(0xFF48dc9a)
+        val defaultColor = Color(0xFFFFFFFF)
+        val iconPadding = Modifier.padding(start = 40.dp, end = 20.dp)
+        val rowModifier = Modifier
+            .fillMaxWidth()
+            .drawBehind {
+                drawLine(
+                    color = Color.White,
+                    start = Offset(0f, 0f),
+                    end = Offset(size.width, 0f),
+                    strokeWidth = 1.dp.toPx(),
+                )
+            }
+            .padding(all = 5.dp)
+        Column(
+            horizontalAlignment = Alignment.Start,
+        ) {
+            Row(
+                modifier = rowModifier
+            ) {
+                Icon(
+                    Icons.Rounded.Call,
+                    contentDescription = "telephone icon",
+                    modifier = iconPadding,
+                    tint = AndroidGreen
+                )
+                Text(
+                    text = "+82 010 1544 3434",
+                    color = defaultColor
+                )
+            }
+            Row(
+                modifier = rowModifier
+            ) {
+                Icon(
+                    Icons.Rounded.Share,
+                    contentDescription = "share icon",
+                    modifier = iconPadding,
+                    tint = AndroidGreen
+                )
+                Text(
+                    text = "@social media link",
+                    color = defaultColor,
+                )
+            }
+            Row(
+                modifier = rowModifier
+            ) {
+                Icon(
+                    Icons.Rounded.Email,
+                    contentDescription = "email icon",
+                    modifier = iconPadding,
+                    tint = AndroidGreen
+                )
+                Text(
+                    text = "email@domain.com",
+                    color = defaultColor,
+                )
+            }
+        }
 }
