99dca091d886450e1ff8ed47c5286baf6e4c8123
DyNamiC <imsong1997@gmail.com>
2022-11-26 18:27:27 +0900

    단원1 - Compose도움말

--- Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -24,12 +22,30 @@ import com.example.composearticle.ui.theme.ComposeArticleTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            ComposeArticleTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    ComposeArticleApp()
+                }
+            }
+        }
     }
 }
 
 @Composable
-fun ComposeArticleApp() { }
+fun ComposeArticleApp() {
+    val title = stringResource(id = R.string.title_jetpack_compose_tutorial)
+    val shortDes = stringResource(id = R.string.compose_short_desc)
+    val longDes = stringResource(id = R.string.compose_long_desc)
+    val img = painterResource(id = R.drawable.bg_compose_background)
+
+    ArticleCard(
+        title = title,
+        shortDescription = shortDes,
+        longDescription = longDes,
+        imagePainter = img
+    )
+}
 
 @Composable
 private fun ArticleCard(
@@ -39,10 +55,51 @@ private fun ArticleCard(
     imagePainter: Painter,
     modifier: Modifier = Modifier,
 ) {
-    Column() { }
+    Column {
+        Image(
+            painter = imagePainter,
+            contentDescription = "Compose Image",
+            modifier = modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+        )
+
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = modifier
+                .padding(start = 16.dp, end = 16.dp, top = 16.dp, bottom = 16.dp)
+        )
+
+        Text(
+            text = shortDescription,
+            modifier = modifier
+                .padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+
+        Text(
+            text = longDescription,
+            modifier = modifier
+                .padding(start = 16.dp, end = 16.dp, top = 16.dp, bottom = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() { }
+fun DefaultPreview() {
+    val title = stringResource(id = R.string.title_jetpack_compose_tutorial)
+    val shortDes = stringResource(id = R.string.compose_short_desc)
+    val longDes = stringResource(id = R.string.compose_long_desc)
+    val img = painterResource(id = R.drawable.bg_compose_background)
+
+    ArticleCard(
+        title = title,
+        shortDescription = shortDes,
+        longDescription = longDes,
+        imagePainter = img
+    )
+}
