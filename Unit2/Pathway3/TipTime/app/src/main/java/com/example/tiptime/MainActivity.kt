1e6563179fce5569bee22e8644f71b83fa5354a8
jpoh281 <hjp4869@naver.com>
2022-11-27 19:25:41 +0900

    자동 테스트 작성 완료

--- Unit2/Pathway3/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Unit2/Pathway3/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.StringRes
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -28,6 +29,7 @@ import com.example.tiptime.ui.theme.TipTimeTheme
 import java.text.NumberFormat
 import androidx.compose.material.SwitchDefaults
 import androidx.compose.ui.graphics.Color
+import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -116,10 +118,11 @@ fun EditNumberField(
         label = { Text(stringResource(id = label)) })
 }
 
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
+@VisibleForTesting
+internal fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
     var tip = tipPercent / 100 * amount
     if (roundUp) tip = kotlin.math.ceil(tip)
-    return NumberFormat.getCurrencyInstance().format(tip)
+    return NumberFormat.getCurrencyInstance(Locale.US).format(tip)
 }
 
 @Composable
---
+++ Unit2/Pathway3/TipTime/app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
@@ -0,0 +1,17 @@
+package com.example.tiptime
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class TipCalculatorTests {
+
+    @Test
+    fun calculate_20_percent_tip_no_round_up() {
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = "$2.00"
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, false)
+        assertEquals(expectedTip, actualTip)
+//        val tipP
+    }
+}
