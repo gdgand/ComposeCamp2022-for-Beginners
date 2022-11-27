907355cf190b40189eec7ca58f84e3f43231c1d8
Atlas <potatoartie@gmail.com>
2022-11-27 20:57:15 +0900

    [add] setContent

--- Unit2/Pathway2/Lomonade/app/src/main/java/com/example/lomonade/MainActivity.kt
+++ Unit2/Pathway2/Lomonade/app/src/main/java/com/example/lomonade/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LomonadeTheme {
                 // A surface container using the 'background' color from the theme
-
+                LemonApp()
             }
         }
     }
