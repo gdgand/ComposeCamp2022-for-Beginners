2b55c21158ca8669923c26233184ff0a5d9e48aa
jpoh281 <hjp4869@naver.com>
2022-11-27 15:44:29 +0900

    새로 레이아웃 만들기

--- Unit2/Pathway2/DiceRoller/app/src/main/java/com/hdd/diceroller/MainActivity.kt
+++ Unit2/Pathway2/DiceRoller/app/src/main/java/com/hdd/diceroller/MainActivity.kt
@@ -3,11 +3,14 @@ package com.hdd.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.hdd.diceroller.ui.theme.DiceRollerTheme
@@ -17,27 +20,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DiceRollerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                DiceRollerApp()
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun DiceRollerApp() {
+    DiceWithButtonAndImage()
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    DiceRollerTheme {
-        Greeting("Android")
+fun DiceWithButtonAndImage(
+    modifier: Modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+) {
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+
     }
 }
