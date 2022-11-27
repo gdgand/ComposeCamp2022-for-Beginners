0332c27f19c50a993817330fee8686443a690e49
jpoh281 <hjp4869@naver.com>
2022-11-27 17:55:01 +0900

    디자인 수정

--- Unit2/Pathway3/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Unit2/Pathway3/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -17,6 +18,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -56,7 +58,13 @@ fun TipTimeScreen() {
 fun EditNumberField() {
     var amountInput by remember { mutableStateOf("") }
 
-    TextField(value = amountInput, onValueChange = { amountInput = it })
+    TextField(
+        modifier = Modifier.fillMaxWidth(),
+        singleLine = true,
+        value = amountInput,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        onValueChange = { amountInput = it },
+        label = { Text(stringResource(id = R.string.cost_of_service)) })
 }
 
 @Preview(showBackground = true)
