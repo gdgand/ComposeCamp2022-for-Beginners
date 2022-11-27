9857083c8bbadd96623162f87aaf2a1905901e68
jpoh281 <hjp4869@naver.com>
2022-11-27 15:46:48 +0900

    버튼 추가

--- Unit2/Pathway2/DiceRoller/app/src/main/java/com/hdd/diceroller/MainActivity.kt
+++ Unit2/Pathway2/DiceRoller/app/src/main/java/com/hdd/diceroller/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.hdd.diceroller.ui.theme.DiceRollerTheme
 
@@ -40,6 +42,8 @@ fun DiceWithButtonAndImage(
         .wrapContentSize(Alignment.Center)
 ) {
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-
+        Button(onClick = { /*TODO*/ }) {
+            Text(stringResource(id = R.string.roll))
+        }
     }
 }
