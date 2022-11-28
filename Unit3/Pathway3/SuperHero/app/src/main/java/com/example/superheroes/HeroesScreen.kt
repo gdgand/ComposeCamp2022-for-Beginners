7e8663f2bfa5a9e11cb38e10a7b6f8b24408473c
KimUJeon <kinfe_411@naver.com>
2022-11-28 15:23:33 +0900

    Super Hero complete

--- Unit3/Pathway3/SuperHero/app/src/main/java/com/example/superheroes/HeroesScreen.kt
+++ Unit3/Pathway3/SuperHero/app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -1,20 +1,4 @@
-/*
- * Copyright (c) 2022 The Android Open Source Project
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
-// This file uses Experimental APIs.
-// Caution: Experimental APIs can change in the future or may be removed entirely.
 @file:OptIn(ExperimentalAnimationApi::class)
 
 package com.example.superheroes
@@ -67,12 +51,10 @@ fun HeroesList(
 ) {
     val visibleState = remember {
         MutableTransitionState(false).apply {
-            // Start the animation immediately.
             targetState = true
         }
     }
 
-    // Fade in entry animation for the entire list
     AnimatedVisibility(
         visibleState = visibleState,
         enter = fadeIn(
@@ -166,10 +148,6 @@ fun HeroesPreview() {
         Surface (
             color = MaterialTheme.colors.background
         ) {
-            /* Important: It is not a good practice to access data source directly from the UI.
-            In later units you will learn how to use ViewModel in such scenarios that takes the
-            data source as a dependency and exposes heroes.
-            */
             HeroesList(heroes = HeroesRepository.heroes)
         }
     }
--- Unit3/Pathway3/SuperHero/app/src/main/java/com/example/superheroes/MainActivity.kt
+++ Unit3/Pathway3/SuperHero/app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright (c) 2022 The Android Open Source Project
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.superheroes
 
 import android.os.Bundle
@@ -46,9 +31,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-/**
- * Composable that displays an app bar and a list of heroes.
- */
 @Composable
 fun SuperheroesApp() {
     Scaffold(
@@ -57,21 +39,12 @@ fun SuperheroesApp() {
             TopAppBar()
         }
     ) {
-        /* Important: It is not a good practice to access data source directly from the UI.
-        In later units you will learn how to use ViewModel in such scenarios that takes the
-        data source as a dependency and exposes heroes.
-         */
         val heroes = HeroesRepository.heroes
         HeroesList(heroes = heroes, Modifier.padding(it))
 
     }
 }
 
-/**
- * Composable that displays a Top Bar with an icon and text.
- *
- * @param modifier modifiers to set to this composable
- */
 @Composable
 fun TopAppBar(modifier: Modifier = Modifier) {
     Box(
