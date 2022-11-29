81a3e0ac671159a3d7abee8ad5210df65495c9d8
kongsubin <qls0919@naver.com>
2022-11-29 17:27:33 +0900

    :art: cupcake 튜토리얼 완료!

--- Unit4/Pathway2/basic-android-kotlin-compose-training-cupcake/app/src/main/java/com/example/cupcake/ui/StartOrderScreen.kt
+++ Unit4/Pathway2/basic-android-kotlin-compose-training-cupcake/app/src/main/java/com/example/cupcake/ui/StartOrderScreen.kt
@@ -35,6 +35,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.cupcake.CupcakeScreen
 import com.example.cupcake.R
 import com.example.cupcake.data.DataSource.quantityOptions
 
@@ -46,6 +47,7 @@ import com.example.cupcake.data.DataSource.quantityOptions
 fun StartOrderScreen(
     quantityOptions: List<Pair<Int, Int>>,
     // TODO: add onNextButtonClicked
+    onNextButtonClicked: (Int) -> Unit,
     modifier: Modifier = Modifier
 ){
     Column(
@@ -65,7 +67,10 @@ fun StartOrderScreen(
         quantityOptions.forEach { item ->
             SelectQuantityButton(
                 labelResourceId = item.first,
-                onClick = { /* TODO: handle next button */ }
+                onClick = {
+                    /* TODO: handle next button */
+                    onNextButtonClicked(item.second)
+                }
             )
         }
     }
@@ -92,5 +97,10 @@ fun SelectQuantityButton(
 @Preview
 @Composable
 fun StartOrderPreview(){
-    StartOrderScreen(quantityOptions = quantityOptions)
+    StartOrderScreen(
+        quantityOptions = quantityOptions,
+        onNextButtonClicked = {
+
+        }
+    )
 }
