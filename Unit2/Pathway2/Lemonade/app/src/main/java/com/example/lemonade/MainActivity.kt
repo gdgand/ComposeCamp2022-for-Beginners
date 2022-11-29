73655ad0b5cf0fa17b436839050e42737fc48fe3
hyeseong <hyeseong7848@gmail.com>
2022-11-29 21:44:07 +0900

    Add: Lemonade - 이미지 클릭 추가

--- Unit2/Pathway2/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ Unit2/Pathway2/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -5,9 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -15,7 +15,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,64 +27,100 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LemonadeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Lemonade(modifier = Modifier
-                        .fillMaxSize()
-                        .wrapContentSize(Alignment.Center))
+                LemonadeApp()
+            }
+        }
+    }
+
+    @Composable
+    fun LemonadeApp() {
+        var step by remember {
+            mutableStateOf(1)
+        }
+
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            when (step) {
+                1 -> {
+                    Lemonade(
+                        stringId = R.string.lemon_text_01,
+                        imgId = R.drawable.lemon_tree,
+                        onImgClick = {
+                            step = 2
+                        }
+                    )
+                }
+                2 -> {
+                    Lemonade(
+                        stringId = R.string.lemon_text_02,
+                        imgId = R.drawable.lemon_squeeze,
+                        onImgClick = {
+                            step = 3
+                        }
+                    )
+                }
+                3 -> {
+                    Lemonade(
+                        stringId = R.string.lemon_text_03,
+                        imgId = R.drawable.lemon_drink,
+                        onImgClick = {
+                            step = 4
+                        }
+                    )
+                }
+                4 -> {
+                    Lemonade(
+                        stringId = R.string.lemon_text_04,
+                        imgId = R.drawable.lemon_restart,
+                        onImgClick = {
+                            step = 1
+                        }
+                    )
                 }
             }
         }
     }
-}
 
-
-@Composable
-fun Lemonade(modifier: Modifier = Modifier) {
-    val step by remember {
-        mutableStateOf(1)
-    }
-    val resultImg = when(step) {
-        1 -> R.drawable.lemon_tree
-        2 -> R.drawable.lemon_squeeze
-        3 -> R.drawable.lemon_drink
-        4 -> R.drawable.lemon_restart
-        else -> R.drawable.lemon_tree
-    }
-
-    val resultString = when(step) {
-        1 -> R.string.lemon_text_01
-        2 -> R.string.lemon_text_02
-        3 -> R.string.lemon_text_03
-        4 -> R.string.lemon_text_04
-        else -> R.string.lemon_text_01
-    }
-
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+    @Composable
+    fun Lemonade(
+        modifier: Modifier = Modifier,
+        stringId: Int,
+        imgId: Int,
+        onImgClick: () -> Unit,
     ) {
-        Text(text = stringResource(resultString), fontSize = 18.sp)
-        Spacer(modifier = Modifier.height(16.dp))
+        Column(
+            modifier = modifier,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = stringResource(id = stringId), fontSize = 18.sp)
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Image(
-            modifier = Modifier.border(2.dp, color = Color(105,205,216), shape = RoundedCornerShape(4)),
-            painter = painterResource(resultImg),
-            contentDescription = step.toString(),
-        )
+            Image(
+                modifier = Modifier
+                    .border(
+                        2.dp,
+                        color = Color(105, 205, 216),
+                        shape = RoundedCornerShape(4)
+                    ).padding(16.dp)
+                    .clickable(
+                        onClick = onImgClick
+                    ),
+                painter = painterResource(imgId),
+                contentDescription = stringResource(id = stringId),
+            )
 
+        }
     }
-}
 
 
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    LemonadeTheme {
-        Lemonade()
+    @Preview(showBackground = true, showSystemUi = true)
+    @Composable
+    fun DefaultPreview() {
+        LemonadeTheme {
+            LemonadeApp()
+        }
     }
 }
