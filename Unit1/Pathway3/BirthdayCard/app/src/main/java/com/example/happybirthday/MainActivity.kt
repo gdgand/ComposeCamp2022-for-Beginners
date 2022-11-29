4013bc6647da23b4e43c2c927f0d4f66b0b50a8a
K_Gs:D <rootachieve6053@gmail.com>
2022-11-29 23:57:25 +0900

    [unit1-path3] 간단한 앱 빌드

--- Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -38,23 +39,45 @@ import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            HappyBirthdayTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    BirthdayGreetingWithText("생일 축하합니다~!", "- from rootachieve")
+                }
+            }
+        }
     }
 }
 
-// 7. 텍스트 정렬 및 패딩 추가
 @Composable
 fun BirthdayGreetingWithText(message: String, from: String) {
-    // Create a column so that texts don't overlap
-    Column { }
+
+    Column {
+        Text(
+            text = message,
+            fontSize = 36.sp
+        )
+
+        Text(
+            text = from,
+            fontSize = 24.sp
+        )
+    }
 }
 
-// 5. Box 레이아웃 추
 @Composable
 fun BirthdayGreetingWithImage(message: String, from: String) { }
 
-// 4. 이미지 컴포저블 추가
-@Preview(showBackground = false)
+@Preview(showBackground = true)
 @Composable
-private fun BirthdayCardPreview() { }
+private fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        BirthdayGreetingWithText("생일 축하합니다~!", "- from rootachieve")
+    }
+}
 
+
+@Composable
+fun Greeting(name: String){
+    Text(text = "Hello $name")
+}
