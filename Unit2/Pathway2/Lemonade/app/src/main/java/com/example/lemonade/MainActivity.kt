86cb4bd0e318cce597d96736693bec25260b55c1
Sujin Kim <sujinkim919@gmail.com>
2022-11-29 20:27:55 +0900

    feat(Unit2/Pathway2/Lemonade): add text and image composable

--- Unit2/Pathway2/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ Unit2/Pathway2/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -3,16 +3,22 @@ package com.example.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +40,21 @@ fun LemonComposable(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        Text(
+            text = stringResource(R.string.lemon_tree),
+            fontSize = 18.sp
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Image(
+            painter = painterResource(R.drawable.lemon_tree),
+            contentDescription = stringResource(R.string.lemon_tree_description),
+            modifier = Modifier
+                .border(
+                    width = 2.dp,
+                    color = Color(105, 205, 216),
+                    shape = RoundedCornerShape(4.dp)
+                )
+        )
     }
 }
 
@@ -42,9 +62,10 @@ fun LemonComposable(
 @Composable
 fun LemonadeApp() {
     LemonadeTheme {
-        LemonComposable(modifier = Modifier
-            .fillMaxSize()
-            .wrapContentSize(Alignment.Center)
+        LemonComposable(
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.Center)
         )
     }
 }
