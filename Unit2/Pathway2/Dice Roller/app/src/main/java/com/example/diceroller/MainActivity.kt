92ed7ad47586a69ac4e2e5c254430eef208af635
ggoma <red3543@naver.com>
2022-11-30 18:48:18 +0900

    DiceRoller 프로젝트 ADD

---
+++ Unit2/Pathway2/Dice Roller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.diceroller.ui.theme.DiceRollerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiceRollerTheme {
+                DiceRollerApp()
+            }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun DiceRollerApp() {
+
+}
+
+@Composable
+fun DiceWithButtonAndImage() {
+    DiceRollerApp()
+}
