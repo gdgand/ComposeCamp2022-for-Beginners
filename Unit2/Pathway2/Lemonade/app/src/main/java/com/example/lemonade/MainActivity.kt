55700563e49e840b5d60ab3fc201292ffe1a59e0
ggoma <red3543@naver.com>
2022-11-30 20:23:12 +0900

    toast메세지 삽입

--- Unit2/Pathway2/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ Unit2/Pathway2/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.lemonade
 
+import android.content.Context
 import android.media.Image
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
@@ -18,6 +20,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role.Companion.Image
@@ -25,12 +28,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
+import kotlinx.coroutines.flow.internal.NoOpContinuation.context
+import kotlin.coroutines.jvm.internal.CompletedContinuation.context
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LemonadeTheme {
+//                WelcomeScreen()
                 LemonApp()
             }
         }
@@ -75,11 +82,7 @@ fun LemonTextAndImage(
 @Composable
 fun LemonApp() {
 
-    // Current step the app is displaying (remember allows the state to be retained
-    // across recompositions).
     var currentStep by remember { mutableStateOf(1) }
-
-    // Number of times the lemon needs to be squeezed to turn into a glass of lemonade
     var squeezeCount by remember { mutableStateOf(0) }
 
     Surface(
@@ -88,31 +91,25 @@ fun LemonApp() {
     ) {
         when (currentStep) {
             1 -> {
-                // Display lemon tree image and ask user to pick a lemon from the tree
                 LemonTextAndImage(
                     textLabelResourceId = R.string.lemon_select,
                     drawableResourceId = R.drawable.lemon_tree,
                     contentDescriptionResourceId = R.string.lemon_tree_content_description,
                     onImageClick = {
-                        // Update to next step
                         currentStep = 2
-                        // Each time a lemon is picked from the tree, get a new random number
-                        // between 2 and 4 (inclusive) for the number of times the lemon needs
-                        // to be squeezed to turn into lemonade
+
                         squeezeCount = (2..4).random()
                     }
                 )
             }
             2 -> {
-                // Display lemon image and ask user to squeeze the lemon
                 LemonTextAndImage(
                     textLabelResourceId = R.string.lemon_squeeze,
                     drawableResourceId = R.drawable.lemon_squeeze,
                     contentDescriptionResourceId = R.string.lemon_content_description,
                     onImageClick = {
-                        // Decrease the squeeze count by 1 for each click the user performs
+//                        Toast.makeText(context, "Squeeez!!", Toast.LENGTH_SHORT).show()
                         squeezeCount--
-                        // When we're done squeezing the lemon, move to the next step
                         if (squeezeCount == 0) {
                             currentStep = 3
                         }
@@ -120,25 +117,21 @@ fun LemonApp() {
                 )
             }
             3 -> {
-                // Display glass of lemonade image and ask user to drink the lemonade
                 LemonTextAndImage(
                     textLabelResourceId = R.string.lemon_drink,
                     drawableResourceId = R.drawable.lemon_drink,
                     contentDescriptionResourceId = R.string.lemonade_content_description,
                     onImageClick = {
-                        // Update to next step
                         currentStep = 4
                     }
                 )
             }
             4 -> {
-                // Display empty glass image and ask user to start again
                 LemonTextAndImage(
                     textLabelResourceId = R.string.lemon_empty_glass,
                     drawableResourceId = R.drawable.lemon_restart,
                     contentDescriptionResourceId = R.string.empty_glass_content_description,
                     onImageClick = {
-                        // Back to starting step
                         currentStep = 1
                     }
                 )
@@ -146,6 +139,7 @@ fun LemonApp() {
         }
     }
 }
+
 @Composable
 fun WelcomeScreen(name: String, onStartClicked: () -> Unit) {
     Column {
