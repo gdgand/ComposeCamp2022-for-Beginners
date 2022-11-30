2508ffd66034bb2ac23a9f710ea2ad9e6fc34453
kkrnl <krnl3321@gmail.com>
2022-11-30 23:42:05 +0900

    Unit1 HappyBirthday

--- Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -16,19 +16,19 @@
 package com.example.happybirthday
 
 import android.os.Bundle
-import android.text.Layout
+//import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.DrawableRes
+//import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+//import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -96,7 +96,6 @@ fun BirthdayGreetingWithImage(message: String, from: String) {
 private fun BirthdayCardPreview() {
     HappyBirthdayTheme {
         BirthdayGreetingWithImage(stringResource(R.string.happy_birthday_text), stringResource(R.string.from))
-        //BirthdayGreetingWithText("Happy Birthday Proro!", "from Pensoo")
     }
 }
 
