c2d31c6e32e8322050e3d0eaaf4951361342d404
ggoma <red3543@naver.com>
2022-11-30 19:16:44 +0900

    Spacer 추가

--- Unit2/Pathway2/Dice Roller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Unit2/Pathway2/Dice Roller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,9 +3,8 @@ package com.example.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -13,9 +12,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -45,6 +47,8 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(painter = painterResource(R.drawable.dice_1), contentDescription = "1")
+        Spacer(modifier = Modifier.height(16.dp))       //spacer 컴포저블
         Button(onClick = { /*TODO*/ }) {
             Text(stringResource(R.string.roll))
         }
