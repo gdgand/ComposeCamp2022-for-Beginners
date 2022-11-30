a951095eccdef3a254065a657a15a5d6c2fc38bb
ggoma <red3543@naver.com>
2022-11-30 19:11:31 +0900

    이미지 추가

--- Unit2/Pathway2/Dice Roller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Unit2/Pathway2/Dice Roller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroller.ui.theme.DiceRollerTheme
@@ -35,10 +38,12 @@ fun DiceRollerApp() {
 @Preview(showBackground = true)
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-
+    Column (
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {}
 }
 
-@Composable
-fun DiceWithButtonAndImage() {
-    DiceRollerApp()
-}
+//@Composable
+//fun DiceWithButtonAndImage() {
+//}
