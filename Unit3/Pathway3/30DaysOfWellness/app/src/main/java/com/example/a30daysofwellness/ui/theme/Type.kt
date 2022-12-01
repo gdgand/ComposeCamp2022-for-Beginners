e14642f3ea188cb81a61b747e03f77aefe820fe3
werwe <werwe.me@gmail.com>
2022-12-01 15:09:23 +0900

    30 days of wellness

---
+++ Unit3/Pathway3/30DaysOfWellness/app/src/main/java/com/example/a30daysofwellness/ui/theme/Type.kt
@@ -0,0 +1,38 @@
+package com.example.a30daysofwellness.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.a30daysofwellness.R
+
+val Sriracha = FontFamily(
+    Font(R.font.sriracha_regular, FontWeight.Normal),
+)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    defaultFontFamily = Sriracha,
+    h1 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 30.sp
+    ),
+    h2 = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    h3 = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
+    ),
+    body1 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    body2 = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    )
+)
