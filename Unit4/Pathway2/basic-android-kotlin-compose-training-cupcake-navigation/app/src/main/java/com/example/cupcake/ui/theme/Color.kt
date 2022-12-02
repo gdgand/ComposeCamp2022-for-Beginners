c99b603a5a38a07d1b83e07a4750cdf775cdd393
crystal <sujeungchoi@gmail.com>
2022-12-03 00:18:49 +0900

    crystal: Home

---
+++ Unit4/Pathway2/basic-android-kotlin-compose-training-cupcake-navigation/app/src/main/java/com/example/cupcake/ui/theme/Color.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.cupcake.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Pink400 = Color(0xFFEC407A)
+val Pink600 = Color(0xFFD81B60)
+val Pink950 = Color(0xFFB31650)
+val Purple400 = Color(0xFFAB47BC)
+val Purple700 = Color(0xFF7B1FA2)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+
