034f0c37838dd5ffd90785003d6c9671a8d9dba6
YuGyeong98 <kuk3097@naver.com>
2022-12-02 22:29:48 +0900

    feat: lemonade 애플리케이션 생성

--- Unit2/Pathway2/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ Unit2/Pathway2/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,24 +24,56 @@ class MainActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
     setContent {
       LemonadeTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-          Greeting("Android")
-        }
+        LemonApp()
       }
     }
   }
 }
 
 @Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
+fun LemonApp() {
+  var currentStep by remember { mutableStateOf(1) }
+  var squeezeCount by remember { mutableStateOf(0) }
+
+  when (currentStep) {
+    1 -> LemonTextAndImage(R.string.lemon_tree_content_description, R.drawable.lemon_tree) {
+      currentStep = 2
+      squeezeCount = (2..4).random()
+    }
+    2 -> LemonTextAndImage(R.string.lemon_content_description, R.drawable.lemon_squeeze) {
+      squeezeCount -= 1
+
+      if (squeezeCount == 0) {
+        currentStep = 3
+      }
+    }
+    3 -> LemonTextAndImage(R.string.glass_of_lemonade_content_description, R.drawable.lemon_drink) {
+      currentStep = 4
+    }
+    4 -> LemonTextAndImage(R.string.empty_glass_content_description, R.drawable.lemon_restart) {
+      currentStep = 1
+    }
+  }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-  LemonadeTheme {
-    Greeting("Android")
+fun LemonTextAndImage(stringId: Int, painterId: Int, onClicked: () -> Unit) {
+  Column(
+    modifier = Modifier.fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center,
+  ) {
+    Text(text = stringResource(stringId), fontSize = 18.sp)
+    Spacer(modifier = Modifier.height(16.dp))
+    Image(
+      painter = painterResource(painterId),
+      contentDescription = stringResource(stringId),
+      modifier = Modifier
+        .wrapContentSize()
+        .border(width = 2.dp, color = Color(105, 205, 216), shape = RoundedCornerShape(4.dp))
+        .clickable {
+          onClicked()
+        }
+    )
   }
 }
