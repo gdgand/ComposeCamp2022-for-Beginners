7c62ce8daf8383bf909026231658b964fd5717af
YuGyeong98 <kuk3097@naver.com>
2022-12-03 00:52:43 +0900

    test: ui 테스트, 기능 테스트 추가

--- Unit2/Pathway3/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Unit2/Pathway3/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.StringRes
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -26,6 +27,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tiptime.ui.theme.TipTimeTheme
 import java.text.NumberFormat
+import java.util.*
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -140,14 +142,19 @@ fun RoundTheTipRow(
   }
 }
 
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
+@VisibleForTesting
+internal fun calculateTip(
+  amount: Double,
+  tipPercent: Double = 15.0,
+  roundUp: Boolean
+): String {
   var tip = tipPercent / 100 * amount
 
   if (roundUp) {
     tip = kotlin.math.ceil(tip)
   }
 
-  return NumberFormat.getCurrencyInstance().format(tip)
+  return NumberFormat.getCurrencyInstance(Locale.US).format(tip)
 }
 
 @Preview(showBackground = true)
