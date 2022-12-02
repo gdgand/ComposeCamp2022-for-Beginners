f15658796956cba3ab0fa0225fdc67bf65bdd8ce
kongsubin <qls0919@naver.com>
2022-12-02 17:59:36 +0900

    :art: reply lab 완료!

---
+++ Unit4/Pathway3/basic-android-kotlin-compose-training-reply-app/app/src/main/java/com/example/reply/ui/utils/WindowStateUtils.kt
@@ -0,0 +1,12 @@
+package com.example.reply.ui.utils
+
+
+// 가로 사이즈에 따른 네이게이션 타입
+enum class ReplyNavigationType {
+    BOTTOM_NAVIGATION, NAVIGATION_RAIL, PERMANENT_NAVIGATION_DRAWER
+}
+
+// 확장형 화면의 목록 세부 정보 뷰를 구현하기 위한 타입
+enum class ReplyContentType {
+    LIST_ONLY, LIST_AND_DETAIL
+}
