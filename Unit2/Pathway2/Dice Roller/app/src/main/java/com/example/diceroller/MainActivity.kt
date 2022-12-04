09f0647031a1b8eb9b87aa59635aa355aaa27794
Parthis Joshi <62930148+Parthis1833@users.noreply.github.com>
2022-12-04 15:46:58 +0530

    complete  unit 2  pathway 2

---
+++ Unit2/Pathway2/Dice Roller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.diceroller.ui.theme.DiceRollerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiceRollerTheme {
+                DiceRollerApp()
+            }
+        }
+    }
+}
+@Preview
+@Composable
+fun DiceRollerApp() {
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+    )
+}
+@Composable
+fun CenterText(){
+    Text(
+         textAlign = TextAlign.Center,
+        text = stringResource(R.string.roll), fontSize = 50.sp)
+
+}
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result by remember { mutableStateOf( 1) }
+    val imageResource = when(result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(modifier = Modifier.clickable{result = (1..6).random()},
+            painter = painterResource(imageResource), contentDescription = result.toString())
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(modifier = Modifier.height(100.dp).width(200.dp),onClick = { result = (1..6).random() }) {
+            CenterText()
+
+        }
+    }
+}
--- Unit2/Pathway3/README.md
+++ Unit2/Pathway3/README.md
@@ -1,20 +1,20 @@
-# Pathway3: UI 및 상태와 상호작용
-Pathway3에서는 총 두가지 프로젝트를 생성합니다.
+# Pathway3: Interacting with UI and state
+Pathway3 creates a total of two projects.
 
-아래 요구사항에 맞게 코드랩을 진행해주세요.
+Please proceed with the code lab according to the requirements below.
 
 <br>
 
 ## Tip Time
 * Project Name : Tip Time
 * Package Name : com.example.tiptime
-* Minimum SDK : API 21 (Lollipop)
-* [참고 코드랩 링크](https://developer.android.com/codelabs/basic-android-kotlin-compose-using-state?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-compose-unit-2-pathway-3%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-compose-using-state#2)
+* Minimum SDK: API 21 (Lollipop)
+* [Reference codelab link](https://developer.android.com/codelabs/basic-android-kotlin-compose-using-state?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways% 2Fandroid-basics-compose-unit-2-pathway-3%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-compose-using-state#2)
 
 <br>
 
 ## Art Space
-* Project Name : Art Space
+* Project Name: Art Space
 * Package Name : com.example.artspace
-* Minimum SDK : API 21 (Lollipop)
-* [참고 코드랩 링크](https://developer.android.com/codelabs/basic-android-kotlin-compose-art-space?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-compose-unit-2-pathway-3%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-compose-art-space#0)
+* Minimum SDK: API 21 (Lollipop)
+* [Reference codelab link](https://developer.android.com/codelabs/basic-android-kotlin-compose-art-space?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways% 2Fandroid-basics-compose-unit-2-pathway-3%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-compose-art-space#0)
