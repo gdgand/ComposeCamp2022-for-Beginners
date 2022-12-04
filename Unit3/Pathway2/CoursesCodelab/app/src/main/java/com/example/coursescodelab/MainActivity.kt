963a222b84d153fa205b51364b2bc7aa83ebc9da
jpoh281 <hjp4869@naver.com>
2022-12-04 19:37:30 +0900

    Courses App 완성

--- Unit3/Pathway2/CoursesCodelab/app/src/main/java/com/example/coursescodelab/MainActivity.kt
+++ Unit3/Pathway2/CoursesCodelab/app/src/main/java/com/example/coursescodelab/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.coursescodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.coursescodelab.data.DataSource
+import com.example.coursescodelab.model.Topic
 import com.example.coursescodelab.ui.theme.CoursesCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +31,9 @@ class MainActivity : ComponentActivity() {
             CoursesCodelabTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CoursesApp()
                 }
             }
         }
@@ -30,14 +41,80 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CoursesApp() {
+    TopicGrid()
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun TopicGrid(modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        cells = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier.padding(8.dp)
+    ) {
+        items(count = DataSource.topics.count()) {
+            TopicCard(topic = DataSource.topics[it])
+        }
+    }
+}
+
+@Composable
+fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
+    Card(elevation = 4.dp) {
+        Row {
+            Box {
+                Image(
+                    modifier = modifier
+                        .height(68.dp)
+                        .width(68.dp)
+                        .aspectRatio(1f),
+                    painter = painterResource(id = topic.imageRes),
+                    contentDescription = stringResource(
+                        id = topic.name
+                    ),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Column {
+                Text(
+                    modifier = Modifier.padding(
+                        top = 16.dp, start = 16.dp, end = 16.dp, bottom = 8.dp
+                    ),
+                    style = MaterialTheme.typography.body2,
+                    text = stringResource(id = topic.name)
+                )
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = "availableCourses",
+                        modifier = Modifier
+                            .padding(start = 16.dp)
+                            .size(12.dp)
+                    )
+                    Text(
+                        text = topic.availableCourses.toString(),
+                        style = MaterialTheme.typography.caption,
+                        modifier = Modifier.padding(start = 8.dp)
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+private fun TopicPreview() {
     CoursesCodelabTheme {
-        Greeting("Android")
+        val topic = Topic(R.string.photography, 321, R.drawable.photography)
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TopicCard(topic = topic)
+        }
     }
 }
