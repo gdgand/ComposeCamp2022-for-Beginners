21f47c9df4ceab39ad38d982fb88ed29c36c2c0c
Charlie_Moon <moon3482@cau.ac.kr>
2022-12-04 23:13:32 +0900

    카드 아이템 추가 레이지 버티컬 그리드 추가

--- Unit3/Pathway2/BuildaGrid/app/src/main/java/com/example/buildagrid/MainActivity.kt
+++ Unit3/Pathway2/BuildaGrid/app/src/main/java/com/example/buildagrid/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.buildagrid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.buildagrid.data.DataSource
+import com.example.buildagrid.model.Topic
 import com.example.buildagrid.ui.theme.BuildaGridTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +31,72 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Scaffold {
+                        TopicList(topicList = DataSource.topics)
+                    }
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TopicList(topicList: List<Topic>) {
+    LazyVerticalGrid(
+        cells = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier.padding(8.dp)
+    ) {
+        items(topicList) { item: Topic ->
+            TopicCard(topic = item)
+        }
+    }
+}
+
+@Composable
+fun TopicCard(topic: Topic) {
+    Card(elevation = 2.dp) {
+        Row(modifier = Modifier.height(68.dp)) {
+            Image(
+                painter = painterResource(id = topic.image),
+                contentDescription = stringResource(
+                    id = topic.stringRes
+                ),
+                modifier = Modifier
+                    .width(68.dp)
+                    .height(68.dp)
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 16.dp, bottom = 8.dp, start = 16.dp, end = 16.dp)
+            ) {
+                Text(
+                    text = stringResource(id = topic.stringRes),
+                    style = MaterialTheme.typography.body2
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = null
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(text = topic.hit.toString(), style = MaterialTheme.typography.caption)
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BuildaGridTheme {
-        Greeting("Android")
+        Scaffold {
+            TopicList(topicList = DataSource.topics)
+        }
     }
 }
