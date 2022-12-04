d53bbb00e59ecc22da9b7c75bf0b4b1703aa4049
Atlas <potatoartie@gmail.com>
2022-12-04 15:50:45 +0900

    [add] compose의 상태소개

--- Unit2/Pathway2/Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Unit2/Pathway2/Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -4,16 +4,23 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.calculator.ui.theme.CalculatorTheme
+import java.text.NumberFormat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    EditNumberField("Android")
+                    TipTimeScreen()
                 }
             }
         }
@@ -34,18 +41,56 @@ class MainActivity : ComponentActivity() {
 
 @SuppressLint("UnrememberedMutableState")
 @Composable
-fun EditNumberField(name: String) {
+fun EditNumberField(
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+
+        TextField(value = value,
+            label = { Text(text = stringResource(id = R.string.cost_of_service))},
+            singleLine = true,
+            onValueChange = onValueChange,
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+
+        )
+}
+
+@Composable
+fun TipTimeScreen() {
     var amountInput by remember {
         mutableStateOf("")
     }
-    Column() {
-        Text(text = "Calculate Tip")
-        TextField(value = amountInput, onValueChange = {
-            amountInput = it
-        })
-    }
 
-    
+    var amount = amountInput.toDoubleOrNull() ?: 0.0
+    var tip = calculateTip(amount)
+
+    Column (
+            modifier = Modifier.padding(32.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+
+            ){
+        Text(text = stringResource(id = R.string.calculate_tip))
+        Spacer(modifier = Modifier.height(16.dp))
+        EditNumberField(value = amountInput,
+            onValueChange = {amountInput = it}
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(
+            text = stringResource(R.string.tip_amount, tip),
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+private fun calculateTip(
+    amount: Double,
+    tipPercent: Double = 15.0
+): String {
+//    var tip = tipPercent / 100 * amount
+    return NumberFormat.getCurrencyInstance().format((tipPercent / 100 * amount))
 }
 
 @Preview(showBackground = true)
@@ -56,7 +101,7 @@ fun DefaultPreview() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colors.background
         ) {
-            EditNumberField("Android")
+            TipTimeScreen()
         }
     }
 }
