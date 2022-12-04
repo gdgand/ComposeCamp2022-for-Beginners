f6103e415120959b9aef96652ec133f1ad5f2058
YuGyeong98 <kuk3097@naver.com>
2022-12-05 01:10:50 +0900

    feat: 랜덤 단어 표시 및 텍스트 필드에 사용자 입력 받기

--- Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/android/unscramble/ui/GameScreen.kt
+++ Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/android/unscramble/ui/GameScreen.kt
@@ -34,6 +34,8 @@ import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -42,102 +44,116 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.android.unscramble.R
 import com.example.android.unscramble.ui.theme.UnscrambleTheme
 
 
 @Composable
-fun GameScreen(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier
-            .verticalScroll(rememberScrollState())
-            .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+fun GameScreen(
+  modifier: Modifier = Modifier,
+  gameViewModel: GameViewModel = viewModel(),
+) {
+  val gameUiState by gameViewModel.uiState.collectAsState()
+
+  Column(
+    modifier = modifier
+      .verticalScroll(rememberScrollState())
+      .padding(16.dp),
+    verticalArrangement = Arrangement.spacedBy(8.dp)
+  ) {
+
+    GameStatus()
+    GameLayout(
+      onUserGuessChanged = { gameViewModel.updateUserGuess(it) },
+      onKeyboardDone = { },
+      currentScrambledWord = gameUiState.currentScrambledWord,
+      userGuess = gameViewModel.userGuess,
+    )
+    Row(
+      modifier = modifier
+        .fillMaxWidth()
+        .padding(top = 16.dp),
+      horizontalArrangement = Arrangement.SpaceAround
     ) {
+      OutlinedButton(
+        onClick = { },
+        modifier = Modifier
+          .weight(1f)
+          .padding(end = 8.dp)
+      ) {
+        Text(stringResource(R.string.skip))
+      }
 
-        GameStatus()
-        GameLayout()
-        Row(
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(top = 16.dp),
-            horizontalArrangement = Arrangement.SpaceAround
-        ) {
-            OutlinedButton(
-                onClick = { },
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(end = 8.dp)
-            ) {
-                Text(stringResource(R.string.skip))
-            }
-
-            Button(
-                modifier = modifier
-                    .fillMaxWidth()
-                    .weight(1f)
-                    .padding(start = 8.dp),
-                onClick = { }
-            ) {
-                Text(stringResource(R.string.submit))
-            }
-        }
+      Button(
+        modifier = modifier
+          .fillMaxWidth()
+          .weight(1f)
+          .padding(start = 8.dp),
+        onClick = { }
+      ) {
+        Text(stringResource(R.string.submit))
+      }
     }
+  }
 }
 
 @Composable
 fun GameStatus(modifier: Modifier = Modifier) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-            .size(48.dp),
-    ) {
-        Text(
-            text = stringResource(R.string.word_count, 0),
-            fontSize = 18.sp,
-        )
-        Text(
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentWidth(Alignment.End),
-            text = stringResource(R.string.score, 0),
-            fontSize = 18.sp,
-        )
-    }
+  Row(
+    modifier = modifier
+      .fillMaxWidth()
+      .padding(16.dp)
+      .size(48.dp),
+  ) {
+    Text(
+      text = stringResource(R.string.word_count, 0),
+      fontSize = 18.sp,
+    )
+    Text(
+      modifier = Modifier
+        .fillMaxWidth()
+        .wrapContentWidth(Alignment.End),
+      text = stringResource(R.string.score, 0),
+      fontSize = 18.sp,
+    )
+  }
 }
 
 @Composable
-fun GameLayout(modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(24.dp),
-
-        ) {
-        Text(
-            text = "scrambleun",
-            fontSize = 45.sp,
-            modifier = modifier.align(Alignment.CenterHorizontally)
-        )
-        Text(
-            text = stringResource(R.string.instructions),
-            fontSize = 17.sp,
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        )
-        OutlinedTextField(
-            value = "",
-            singleLine = true,
-            modifier = Modifier.fillMaxWidth(),
-            onValueChange = { },
-            label = { Text(stringResource(R.string.enter_your_word)) },
-            isError = false,
-            keyboardOptions = KeyboardOptions.Default.copy(
-                imeAction = ImeAction.Done
-            ),
-            keyboardActions = KeyboardActions(
-                onDone = { }
-            ),
-        )
-    }
+fun GameLayout(
+  onUserGuessChanged: (String) -> Unit,
+  onKeyboardDone: () -> Unit,
+  modifier: Modifier = Modifier,
+  currentScrambledWord: String,
+  userGuess: String,
+  ) {
+  Column(verticalArrangement = Arrangement.spacedBy(24.dp)) {
+    Text(
+      text = currentScrambledWord,
+      fontSize = 45.sp,
+      modifier = modifier.align(Alignment.CenterHorizontally)
+    )
+    Text(
+      text = stringResource(R.string.instructions),
+      fontSize = 17.sp,
+      modifier = Modifier.align(Alignment.CenterHorizontally)
+    )
+    OutlinedTextField(
+      value = userGuess,
+      singleLine = true,
+      modifier = Modifier.fillMaxWidth(),
+      onValueChange = onUserGuessChanged,
+      label = { Text(stringResource(R.string.enter_your_word)) },
+      isError = false,
+      keyboardOptions = KeyboardOptions.Default.copy(
+        imeAction = ImeAction.Done
+      ),
+      keyboardActions = KeyboardActions(
+        onDone = { onKeyboardDone() }
+      ),
+    )
+  }
 }
 
 /*
@@ -145,43 +161,43 @@ fun GameLayout(modifier: Modifier = Modifier) {
  */
 @Composable
 private fun FinalScoreDialog(
-    onPlayAgain: () -> Unit,
-    modifier: Modifier = Modifier
+  onPlayAgain: () -> Unit,
+  modifier: Modifier = Modifier
 ) {
-    val activity = (LocalContext.current as Activity)
+  val activity = (LocalContext.current as Activity)
 
-    AlertDialog(
-        onDismissRequest = {
-            // Dismiss the dialog when the user clicks outside the dialog or on the back
-            // button. If you want to disable that functionality, simply use an empty
-            // onCloseRequest.
-        },
-        title = { Text(stringResource(R.string.congratulations)) },
-        text = { Text(stringResource(R.string.you_scored, 0)) },
-        modifier = modifier,
-        dismissButton = {
-            TextButton(
-                onClick = {
-                    activity.finish()
-                }
-            ) {
-                Text(text = stringResource(R.string.exit))
-            }
-        },
-        confirmButton = {
-            TextButton(
-                onClick = onPlayAgain
-            ) {
-                Text(text = stringResource(R.string.play_again))
-            }
+  AlertDialog(
+    onDismissRequest = {
+      // Dismiss the dialog when the user clicks outside the dialog or on the back
+      // button. If you want to disable that functionality, simply use an empty
+      // onCloseRequest.
+    },
+    title = { Text(stringResource(R.string.congratulations)) },
+    text = { Text(stringResource(R.string.you_scored, 0)) },
+    modifier = modifier,
+    dismissButton = {
+      TextButton(
+        onClick = {
+          activity.finish()
         }
-    )
+      ) {
+        Text(text = stringResource(R.string.exit))
+      }
+    },
+    confirmButton = {
+      TextButton(
+        onClick = onPlayAgain
+      ) {
+        Text(text = stringResource(R.string.play_again))
+      }
+    }
+  )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    UnscrambleTheme {
-        GameScreen()
-    }
+  UnscrambleTheme {
+    GameScreen()
+  }
 }
---
+++ Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
@@ -0,0 +1,58 @@
+package com.example.android.unscramble.ui
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.android.unscramble.data.allWords
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class GameViewModel : ViewModel() {
+  private val _uiState = MutableStateFlow(GameUiState())
+  private var _count = 0
+  private lateinit var currentWord: String
+  private var usedWords: MutableSet<String> = mutableSetOf()
+
+  val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+  val count: Int
+    get() = _count
+  var userGuess by mutableStateOf("")
+    private set
+
+  init {
+    resetGame()
+  }
+
+  fun updateUserGuess(guessedWord: String){
+    userGuess = guessedWord
+  }
+
+  private fun resetGame() {
+    usedWords.clear()
+    _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+  }
+
+  private fun pickRandomWordAndShuffle(): String {
+    currentWord = allWords.random()
+
+    return if (usedWords.contains(currentWord)) {
+      pickRandomWordAndShuffle()
+    } else {
+      usedWords.add(currentWord)
+      shuffleCurrentWord(currentWord)
+    }
+  }
+
+  private fun shuffleCurrentWord(word: String): String {
+    val tempWord = word.toCharArray()
+
+    tempWord.shuffle()
+    while (String(tempWord) == word) {
+      tempWord.shuffle()
+    }
+
+    return String(tempWord)
+  }
+}
