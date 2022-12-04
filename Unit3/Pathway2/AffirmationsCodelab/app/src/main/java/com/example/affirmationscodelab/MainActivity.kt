4a42f14db6593c4c378a2cbb5e66301540120966
Charlie_Moon <moon3482@cau.ac.kr>
2022-12-04 21:48:14 +0900

    프리뷰 작성

--- Unit3/Pathway2/AffirmationsCodelab/app/src/main/java/com/example/affirmationscodelab/MainActivity.kt
+++ Unit3/Pathway2/AffirmationsCodelab/app/src/main/java/com/example/affirmationscodelab/MainActivity.kt
@@ -29,8 +29,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.affirmationscodelab.model.Affirmation
 import com.example.affirmationscodelab.ui.theme.AffirmationsTheme
@@ -70,4 +72,11 @@ fun AffirmationApp() {
     val context = LocalContext.current
     AffirmationsTheme {
     }
+}
+
+@Preview
+@Composable
+private fun AffirmationCardPreview() {
+    AffirmationCard(affirmation = Affirmation(R.string.affirmation1, R.drawable.image1))
+
 }
