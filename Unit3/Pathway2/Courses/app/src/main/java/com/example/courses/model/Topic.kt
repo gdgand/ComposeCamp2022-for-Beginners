816c154e78418608810382a20f585306d48647bb
sucrechat <akanetail@gmail.com>
2022-12-05 18:30:47 +0900

    연습: 그리드 빌드

---
+++ Unit3/Pathway2/Courses/app/src/main/java/com/example/courses/model/Topic.kt
@@ -0,0 +1,8 @@
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Topic(
+    @StringRes val name: Int,
+    val availableCourses: Int,
+    @DrawableRes val imageRes: Int
+)
