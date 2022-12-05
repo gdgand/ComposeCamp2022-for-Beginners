fe142309f2851196326931e5209731340c44f568
supernovel <ekfueh@naver.com>
2022-12-05 22:16:58 +0900

    feat: 텍스트 리소스 추가

--- Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ Unit1/Pathway3/BirthdayCard/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -41,7 +41,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HappyBirthdayTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    BirthdayGreetingWithImage( "Happy Birthday Sam!", "- from Emma")
+                    BirthdayGreetingWithImage( getString(R.string.happy_birthday_text), getString(R.string.signature_text))
                 }
             }
         }
