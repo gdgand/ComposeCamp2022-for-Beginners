1cc01f38a335fc6941e4c12209c986eb0d6aedeb
Sujin Kim <sujinkim919@gmail.com>
2022-12-07 02:41:42 +0900

    feat(Unit2/Pathway3/TipTime): set action button

--- Unit2/Pathway3/TeaTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Unit2/Pathway3/TeaTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -62,12 +63,20 @@ fun TipTimeScreen() {
         EditNumberField(
             label = R.string.bill_amount,
             value = amountInput,
-            onValueChange = { amountInput = it }
+            onValueChange = { amountInput = it },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            )
         )
         EditNumberField(
             label = R.string.how_was_the_service,
             value = tipInput,
-            onValueChange = { tipInput = it }
+            onValueChange = { tipInput = it },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            )
         )
         Spacer(modifier = Modifier.height(24.dp))
         Text(
@@ -84,7 +93,8 @@ fun EditNumberField(
     @StringRes label: Int,
     value: String,
     onValueChange: (String) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    keyboardOptions: KeyboardOptions
 ) {
     TextField(
         value = value, // 전달하는 문자열 값
@@ -92,7 +102,7 @@ fun EditNumberField(
         label = { Text(stringResource(label)) },
         modifier = Modifier.fillMaxWidth(),
         singleLine = true,   // 가로로 스크롤 가능한 하나의 줄로 압축
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        keyboardOptions = keyboardOptions
     )
 }
 
