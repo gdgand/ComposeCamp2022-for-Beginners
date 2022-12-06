770669ba009cb5ce33e32d8cd622ee53f460ed89
Sujin Kim <sujinkim919@gmail.com>
2022-12-07 01:28:39 +0900

    feat(Unit2/Pathway3/TipTime): add text field (edit text)

--- Unit2/Pathway3/TeaTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Unit2/Pathway3/TeaTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -3,17 +3,17 @@ package com.example.tiptime
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -41,13 +41,35 @@ fun TipTimeScreen() {
     Column(
         modifier = Modifier.padding(32.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
-    ){
+    ) {
         Text(
             text = stringResource(id = R.string.calculate_tip),
             fontSize = 24.sp,
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
+
+        Spacer(modifier = Modifier.height(16.dp))
+        EditNumberField()
     }
+}
+
+@Composable
+fun EditNumberField() {
+    // var amountInput = mutableStateOf("0")
+    var amountInput by remember { mutableStateOf("") }  // 관찰 가능
+
+    TextField(
+        value = amountInput, // 전달하는 문자열 값
+        onValueChange = { // 텍스트 입력 시 트리거되는 람다 콜백
+            amountInput = it
+        },
+        label = {
+            Text(stringResource(id = R.string.cost_of_service))
+        },
+        modifier = Modifier.fillMaxWidth(),
+        singleLine = true,   // 가로로 스크롤 가능한 하나의 줄로 압축
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
 }
 
 @Preview(showBackground = true)
