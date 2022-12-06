dba3fa94552a5a06970fe9e6d9a7bc8df4e8ea23
Parthis Joshi <62930148+Parthis1833@users.noreply.github.com>
2022-12-06 16:00:39 +0530

    unit3 pathway2 affirmationcodelab completed

--- Unit3/Pathway2/AffirmationsCodelab/app/src/main/java/com/example/affirmationscodelab/data/Datasource.kt
+++ Unit3/Pathway2/AffirmationsCodelab/app/src/main/java/com/example/affirmationscodelab/data/Datasource.kt
@@ -1,26 +1,12 @@
-/*
- * Copyright (C) 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.affirmationscodelab.data
 import com.example.affirmationscodelab.R
+import com.example.affirmationscodelab.model.Affirmation
 
 /**
  * [Datasource] generates a list of [Affirmation]
  */
 class Datasource() {
-    /*fun loadAffirmations(): List<Affirmation> {
+    fun loadAffirmations(): List<Affirmation> {
         return listOf<Affirmation>(
             Affirmation(R.string.affirmation1, R.drawable.image1),
             Affirmation(R.string.affirmation2, R.drawable.image2),
@@ -32,5 +18,5 @@ class Datasource() {
             Affirmation(R.string.affirmation8, R.drawable.image8),
             Affirmation(R.string.affirmation9, R.drawable.image9),
             Affirmation(R.string.affirmation10, R.drawable.image10))
-    }*/
+    }
 }
