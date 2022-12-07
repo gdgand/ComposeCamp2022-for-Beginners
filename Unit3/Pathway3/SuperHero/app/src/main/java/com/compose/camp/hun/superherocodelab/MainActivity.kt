13e4a869226fa6efb0eaddc64254b3a82a4584ab
dale.band <dale.band@kakaocorp.com>
2022-12-07 21:40:48 +0900

    [Unit3 Pathway3] super hero first commit

---
+++ Unit3/Pathway3/SuperHero/app/src/main/java/com/compose/camp/hun/superherocodelab/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.compose.camp.hun.superherocodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.compose.camp.hun.superherocodelab.ui.theme.SuperHeroCodeLabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SuperHeroCodeLabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SuperHeroCodeLabTheme {
+        Greeting("Android")
+    }
+}
