b870ab62127982a947dcd73ffc34a48e6a019a0f
april <aprilgom@gmail.com>
2022-12-07 15:16:55 +0900

    Focus added

--- Unit2/Pathway3/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Unit2/Pathway3/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection.Companion.Down
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
@@ -46,6 +49,7 @@ fun TipTimeScreen() {
     val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
     val tip = calculateTips(amount,tipPercent)
 
+    val focusManager = LocalFocusManager.current
 
     Column (
         modifier = Modifier
@@ -64,6 +68,9 @@ fun TipTimeScreen() {
                 keyboardType = KeyboardType.Number,
                 imeAction = ImeAction.Next
             ),
+            keyboardActions = KeyboardActions(
+                onNext = {focusManager.moveFocus(Down)}
+            ),
             value = amountInput,
             onValueChange = { amountInput = it}
         )
@@ -73,6 +80,9 @@ fun TipTimeScreen() {
             keyboardOptions = KeyboardOptions.Default.copy(
                 keyboardType = KeyboardType.Number,
                 imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {focusManager.clearFocus()},
             ),
             value = tipInput,
             onValueChange = { tipInput = it}
@@ -92,6 +102,7 @@ fun TipTimeScreen() {
 fun EditNumberField(
     @StringRes label: Int,
     keyboardOptions: KeyboardOptions,
+    keyboardActions: KeyboardActions,
     value: String,
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier
@@ -107,6 +118,7 @@ fun EditNumberField(
         },
         singleLine = true,
         keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
     )
 }
 
