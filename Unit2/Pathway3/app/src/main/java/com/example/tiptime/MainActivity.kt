eba908ead3ba39e61fa32a00c8cefe530671cd43
aprilgom <aprilgom@gmail.com>
2022-12-07 09:21:24 +0900

    appearance modified

--- Unit2/Pathway3/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Unit2/Pathway3/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -13,6 +14,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -52,10 +54,17 @@ fun EditNumberField() {
         mutableStateOf("0")
     }
     TextField(
+        modifier = Modifier
+            .fillMaxWidth(),
         value = amountInput,
         onValueChange = {
             amountInput = it
         },
+        label = {
+          Text(text = stringResource(id = R.string.cost_of_service))
+        },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
     )
 }
 
