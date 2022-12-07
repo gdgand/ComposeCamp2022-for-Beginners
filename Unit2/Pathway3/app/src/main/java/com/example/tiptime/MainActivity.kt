23c17603784f2df31953807cb24b3a30d13ad703
aprilgom <aprilgom@gmail.com>
2022-12-07 09:36:28 +0900

    tip amount displayed

--- Unit2/Pathway3/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Unit2/Pathway3/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -46,6 +47,13 @@ fun TipTimeScreen() {
         )
         Spacer(Modifier.height(16.dp))
         EditNumberField()
+        Spacer(Modifier.height(24.dp))
+        Text(
+            text = stringResource(id = R.string.tip_amount, ""),
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
