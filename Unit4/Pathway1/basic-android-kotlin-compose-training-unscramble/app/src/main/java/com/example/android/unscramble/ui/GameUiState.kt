6f92857d005fc4f94fc01efb9d5cd2675b13d0f8
Charlie_Moon <moon3482@cau.ac.kr>
2022-12-07 22:04:16 +0900

    스코어, 현재 글자수, 게임오버 상태 추가

--- Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/android/unscramble/ui/GameUiState.kt
+++ Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/android/unscramble/ui/GameUiState.kt
@@ -3,4 +3,7 @@ package com.example.android.unscramble.ui
 data class GameUiState(
     val currentScrambledWord: String = "",
     val isGuessedWordWrong: Boolean = false,
+    val score: Int = 0,
+    val currentWordCount: Int = 0,
+    val isGameOver: Boolean = false,
 )
