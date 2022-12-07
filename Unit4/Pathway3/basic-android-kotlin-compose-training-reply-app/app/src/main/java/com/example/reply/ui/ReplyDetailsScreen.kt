3d8afd184c83777f5da078683305d7494876a47a
Kwon Myeongeun <kmyeongeun95@gmail.com>
2022-12-07 20:36:12 +0900

    ReplyApp 완료

--- Unit4/Pathway3/basic-android-kotlin-compose-training-reply-app/app/src/main/java/com/example/reply/ui/ReplyDetailsScreen.kt
+++ Unit4/Pathway3/basic-android-kotlin-compose-training-reply-app/app/src/main/java/com/example/reply/ui/ReplyDetailsScreen.kt
@@ -17,12 +17,15 @@
 package com.example.reply.ui
 
 import android.widget.Toast
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -42,6 +45,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.reply.R
@@ -56,19 +60,30 @@ fun ReplyDetailsScreen(
     replyUiState: ReplyUiState,
     modifier: Modifier = Modifier,
     onBackPressed: () -> Unit = {},
+    isFullScreen: Boolean = false
 ) {
+    BackHandler {
+        onBackPressed()
+    }
     LazyColumn(
         modifier = modifier
+            .testTag(stringResource(R.string.details_screen))
             .fillMaxSize()
             .background(color = MaterialTheme.colorScheme.inverseOnSurface)
             .padding(top = 24.dp)
     ) {
         item {
-            ReplyDetailsScreenTopBar(onBackPressed, replyUiState)
+            if (isFullScreen) {
+                ReplyDetailsScreenTopBar(onBackPressed, replyUiState)
+            }
             ReplyEmailDetailsCard(
                 email = replyUiState.currentSelectedEmail,
                 mailboxType = replyUiState.currentMailbox,
-                modifier = Modifier.padding(horizontal = 16.dp)
+                isFullScreen = isFullScreen,
+                modifier = if (isFullScreen)
+                    Modifier.padding(horizontal = 16.dp)
+                else
+                    Modifier.padding(end = 16.dp)
             )
         }
     }
@@ -118,6 +133,7 @@ private fun ReplyEmailDetailsCard(
     email: Email,
     mailboxType: MailboxType,
     modifier: Modifier = Modifier,
+    isFullScreen: Boolean = false
 ) {
     val context = LocalContext.current
     val displayToast = { text: String ->
@@ -133,12 +149,16 @@ private fun ReplyEmailDetailsCard(
                 .padding(20.dp)
         ) {
             DetailsScreenHeader(email)
-            Text(
-                text = email.subject,
-                style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.outline,
-                modifier = Modifier.padding(top = 12.dp, bottom = 8.dp),
-            )
+            if (!isFullScreen) {
+                Text(
+                    text = email.subject,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.outline,
+                    modifier = Modifier.padding(top = 12.dp, bottom = 8.dp),
+                )
+            } else {
+                Spacer(modifier = Modifier.height(12.dp))
+            }
             Text(
                 text = email.body,
                 style = MaterialTheme.typography.bodyLarge,
