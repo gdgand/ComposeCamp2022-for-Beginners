27d6a46997d184c64e8122d7274bb9fb56fb6cd6
jeong-1 <wjddms680@naver.com>
2022-12-08 03:20:58 +0900

    Complete: Practice Affirmation

--- Unit3/Pathway2/AffirmationsCodelab/app/src/main/java/com/example/affirmationscodelab/MainActivity.kt
+++ Unit3/Pathway2/AffirmationsCodelab/app/src/main/java/com/example/affirmationscodelab/MainActivity.kt
@@ -17,15 +17,39 @@ package com.example.affirmationscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.affirmationscodelab.data.Datasource
+import com.example.affirmationscodelab.model.Affirmation
 import com.example.affirmationscodelab.ui.theme.AffirmationsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AffirmationApp()
+            Surface(
+                color = MaterialTheme.colors.background
+            ) {
+                AffirmationApp()
+            }
         }
     }
 }
@@ -33,5 +57,48 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AffirmationApp() {
     AffirmationsTheme {
+        AffirmationsTheme {
+            AffirmationList(affirmationList = Datasource().loadAffirmations())
+        }
+    }
+}
+
+@Composable
+fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
+    LazyColumn {
+        items(affirmationList) { affirmation ->
+            AffirmationCard(affirmation)
+        }
+    }
+}
+
+@Composable
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
+    Column {
+        Image(
+            painter = painterResource(affirmation.imageResourceId),
+            contentDescription = stringResource(affirmation.stringResourceId),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(194.dp),
+            contentScale = ContentScale.Crop
+        )
+        Text(
+            text = LocalContext.current.getString(affirmation.stringResourceId),
+            modifier = Modifier.padding(16.dp),
+            style = MaterialTheme.typography.h6
+        )
+    }
+}
+
+@Preview
+@Composable
+fun AffirmationCardPreview() {
+    AffirmationsTheme {
+        Surface(
+            color = MaterialTheme.colors.background
+        ) {
+            AffirmationCard(affirmation = Affirmation(R.string.affirmation1, R.drawable.image1))
+        }
     }
 }
