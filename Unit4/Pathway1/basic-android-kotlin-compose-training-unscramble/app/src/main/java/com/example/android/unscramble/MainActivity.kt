343c421d387bccf2ea8ac7c49f62371da81b9ac1
Charlie_Moon <moon3482@cau.ac.kr>
2022-12-07 21:18:02 +0900

    컴포저블 작성 GameStatus GameLayout GameScreen FinalScoreDialog

--- Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/android/unscramble/MainActivity.kt
+++ Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/android/unscramble/MainActivity.kt
@@ -16,13 +16,25 @@
 
 package com.example.android.unscramble
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.example.android.unscramble.ui.GameScreen
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.android.unscramble.ui.theme.UnscrambleTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,5 +52,125 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    @Composable
+    fun GameStatus(modifier: Modifier = Modifier) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .size(48.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.word_count, 0),
+                fontSize = 18.sp
+            )
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentWidth(Alignment.End),
+                text = stringResource(id = R.string.score, 0),
+                fontSize = 18.sp
+            )
+        }
+    }
+
+    @Composable
+    fun GameLayout(modifier: Modifier = Modifier) {
+        Column(verticalArrangement = Arrangement.spacedBy(24.dp)) {
+            Text(
+                text = "",
+                fontSize = 45.sp,
+                modifier = modifier.align(Alignment.CenterHorizontally)
+            )
+            Text(
+                text = stringResource(id = R.string.instructions),
+                fontSize = 17.sp,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(text = stringResource(id = R.string.enter_your_word)) },
+                isError = false,
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = {})
+            )
+        }
+    }
+
+    @Composable
+    fun GameScreen(modifier: Modifier = Modifier) {
+        Column(
+            modifier = modifier
+                .verticalScroll(
+                    rememberScrollState()
+                )
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            GameStatus()
+            GameLayout()
+            Row(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                OutlinedButton(
+                    onClick = { /*TODO*/ }, modifier = Modifier
+                        .weight(1f)
+                        .padding(end = 8.dp)
+                ) {
+                    Text(text = stringResource(id = R.string.skip))
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .padding(start = 8.dp)
+                ) {
+                    Text(text = stringResource(id = R.string.submit))
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun FinalScoreDialog(
+        onPlayAgain: () -> Unit,
+        modifier: Modifier = Modifier
+    ) {
+        val activity = (LocalContext.current as Activity)
+
+        AlertDialog(
+            onDismissRequest = { /*TODO*/ },
+            title = { Text(text = stringResource(id = R.string.congratulations)) },
+            text = { Text(text = stringResource(id = R.string.you_scored, 0)) },
+            modifier = modifier,
+            dismissButton = {
+                TextButton(onClick = { activity.finish() }) {
+                    Text(text = stringResource(id = R.string.exit))
+                }
+            },
+            confirmButton = {
+                TextButton(onClick = { onPlayAgain }) {
+                    Text(text = stringResource(id = R.string.play_again))
+                }
+            }
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun UnscramblePreview() {
+        UnscrambleTheme {
+            GameScreen()
+        }
+    }
+
 }
 
