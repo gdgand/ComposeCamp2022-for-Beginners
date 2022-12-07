ceb7639bfb3e386c6f99e2e93f980b296911eaed
aprilgom <aprilgom@gmail.com>
2022-12-07 09:11:00 +0900

    save state with remember

--- Unit2/Pathway3/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Unit2/Pathway3/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tiptime
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,7 +9,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -44,12 +45,17 @@ fun TipTimeScreen() {
         EditNumberField()
     }
 }
+
 @Composable
 fun EditNumberField() {
-    val amountInput = "0"
+    var amountInput by remember {
+        mutableStateOf("0")
+    }
     TextField(
         value = amountInput,
-        onValueChange = {}
+        onValueChange = {
+            amountInput = it
+        },
     )
 }
 
