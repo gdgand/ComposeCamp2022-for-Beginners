75069e9ed65b44c32af3cf56d77a0e9a6b4b10c5
supernovel <ekfueh@naver.com>
2022-12-08 23:53:37 +0900

    fix: 스타일 빠진 부분 추가

--- Unit2/Pathway2/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ Unit2/Pathway2/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,18 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerBasedShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -59,7 +64,7 @@ fun LemonadeApp() {
                 action = {
                     squeezeTimes -= 1
 
-                    if(squeezeTimes == 0) {
+                    if (squeezeTimes == 0) {
                         currentStep = 3
                     }
                 }
@@ -99,14 +104,19 @@ fun LemonadeStepItem(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(description)
+        Text(description, fontSize = 18.sp)
         Spacer(modifier = Modifier.height(16.dp))
         Image(
             painterResource(id = imageResource),
             contentDescription = imageDescription,
-            modifier = Modifier.clickable {
-                action()
-            },
+            modifier = Modifier
+                .border(
+                    width = 2.dp, color = Color(105, 205, 216),
+                    shape = RoundedCornerShape(4.dp)
+                )
+                .clickable {
+                    action()
+                },
         )
     }
 }
