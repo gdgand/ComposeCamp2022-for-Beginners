18f443dea2328938e8b1a18827560fa938ab3f92
Yuhee Kim <dbgml4509@naver.com>
2022-12-09 22:09:46 +0900

    feat : UI 설정 함수 onCreate()에서 호출

--- Unit1/Pathway3/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ Unit1/Pathway3/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -25,7 +25,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            ComposeQuadrantTheme {
+                Surface(color = MaterialTheme.colors.background){
+                    ComposeQuadrantApp()
+                }
+            }
         }
     }
 }
