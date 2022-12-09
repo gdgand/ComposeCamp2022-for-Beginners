05f48273a750fe42a8ec61b50c04bc2893f50ffe
Yuhee Kim <dbgml4509@naver.com>
2022-12-09 19:52:52 +0900

    feat : 텍스트, 이미지 레이아웃 설정 함수 Activity에서 호출

--- Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -25,7 +25,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            ComposeArticleTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    ComposeArticleApp()
+                }
+            }
         }
     }
 }
