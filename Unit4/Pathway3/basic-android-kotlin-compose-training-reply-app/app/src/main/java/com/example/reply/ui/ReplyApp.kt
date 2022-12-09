a6fb4487799d73c7bd08840dcdc993f935264a2d
msjjang <dewlain@gmail.com>
2022-12-09 14:02:57 +0900

    add tablet mode

--- Unit4/Pathway3/basic-android-kotlin-compose-training-reply-app/app/src/main/java/com/example/reply/ui/ReplyApp.kt
+++ Unit4/Pathway3/basic-android-kotlin-compose-training-reply-app/app/src/main/java/com/example/reply/ui/ReplyApp.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.reply.data.Email
 import com.example.reply.data.MailboxType
+import com.example.reply.ui.utils.ReplyNavigationType
 
 /**
  * Main composable that serves as container
@@ -35,22 +36,24 @@ fun ReplyApp(
 ) {
     val viewModel: ReplyViewModel = viewModel()
     val replyUiState = viewModel.uiState.collectAsState().value
+    var navigationType : ReplyNavigationType
 
     when (windowSize) {
         WindowWidthSizeClass.Compact -> {
-
+            navigationType = ReplyNavigationType.BOTTOM_NAVIGATION
         }
         WindowWidthSizeClass.Medium -> {
-
+            navigationType = ReplyNavigationType.NAVIGATION_RAIL
         }
         WindowWidthSizeClass.Expanded -> {
-
+            navigationType = ReplyNavigationType.PERMANENT_NAVIGATION
         }
         else -> {
-
+            navigationType = ReplyNavigationType.BOTTOM_NAVIGATION
         }
     }
     ReplyHomeScreen(
+        navigationType = navigationType,
         replyUiState = replyUiState,
         onTabPressed = { mailboxType: MailboxType ->
             viewModel.updateCurrentMailbox(mailboxType = mailboxType)
