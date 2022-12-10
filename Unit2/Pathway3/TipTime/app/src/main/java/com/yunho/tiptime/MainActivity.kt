757edfa9682b32b9b44bda30c9eb123407749928
DyNamiC <imsong1997@gmail.com>
2022-12-10 17:38:17 +0900

    단원2 - TipTime init

---
+++ Unit2/Pathway3/TipTime/app/src/main/java/com/yunho/tiptime/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.yunho.tiptime
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.yunho.tiptime.ui.theme.TipTimeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TipTimeTheme {
+                TipTimeApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun TipTimeApp() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TipTimeTheme {
+        TipTimeApp()
+    }
+}
