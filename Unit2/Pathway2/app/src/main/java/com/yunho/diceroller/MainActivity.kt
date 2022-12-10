add7a5dca28615ffb354a2570f906771aa821d13
DyNamiC <imsong1997@gmail.com>
2022-12-10 17:06:49 +0900

    Dice Roller

--- Unit2/Pathway2/app/src/main/java/com/yunho/diceroller/MainActivity.kt
+++ Unit2/Pathway2/app/src/main/java/com/yunho/diceroller/MainActivity.kt
@@ -3,13 +3,17 @@ package com.yunho.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.yunho.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +21,57 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DiceRollerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                DiceRollerApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun DiceRollerApp() {
+    DiceWithButtonAndImage(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    )
 }
+
+
+@Composable
+fun DiceWithButtonAndImage(
+    modifier: Modifier = Modifier
+) {
+    var result by remember { mutableStateOf( 1) }
+
+    val resource = when(result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    Column (
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(resource), contentDescription = "Dice - $result")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = { result = rollDice() }) {
+            Text(stringResource(R.string.roll))
+        }
+    }
+}
+
+private fun rollDice(): Int = (1..6).random()
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun DiceRollerAppPreview() {
     DiceRollerTheme {
-        Greeting("Android")
+        DiceRollerApp()
     }
 }
