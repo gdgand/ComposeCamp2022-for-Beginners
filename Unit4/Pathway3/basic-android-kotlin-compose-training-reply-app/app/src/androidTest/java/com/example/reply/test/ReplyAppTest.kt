3b8cd5b7d90726dcd2b3e39410118cf92bb7c7e5
Charlie_Moon <moon3482@cau.ac.kr>
2022-12-10 21:45:42 +0900

    테스트 코드 작성 어노테이션 추가

--- Unit4/Pathway3/basic-android-kotlin-compose-training-reply-app/app/src/androidTest/java/com/example/reply/test/ReplyAppTest.kt
+++ Unit4/Pathway3/basic-android-kotlin-compose-training-reply-app/app/src/androidTest/java/com/example/reply/test/ReplyAppTest.kt
@@ -3,10 +3,10 @@ package com.example.reply.test
 import androidx.activity.ComponentActivity
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import com.example.reply.R
 import com.example.reply.ui.ReplyApp
 import org.junit.Rule
 import org.junit.Test
-import com.example.reply.R
 
 class ReplyAppTest {
 
@@ -14,8 +14,9 @@ class ReplyAppTest {
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
     @Test
+    @TestCompactWidth
     fun compactDevice_verifyUsingBottomNavigation() {
-        // Set up compact window
+
         composeTestRule.setContent {
             ReplyApp(
                 windowSize = WindowWidthSizeClass.Compact
@@ -27,6 +28,7 @@ class ReplyAppTest {
     }
 
     @Test
+    @TestMediumWidth
     fun mediumDevice_verifyUsingNavigationRail() {
         composeTestRule.setContent {
             ReplyApp(
@@ -39,6 +41,7 @@ class ReplyAppTest {
     }
 
     @Test
+    @TestExpandedWidth
     fun expandedDevice_verifyUsingNavigationDrawer() {
         composeTestRule.setContent {
             ReplyApp(
