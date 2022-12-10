156d61402e15f8939e72a25adb3ed63f8fb7d34d
dale.band <dale.band@kakaocorp.com>
2022-12-10 14:26:46 +0900

    [Unit3 Pathway3] MaterialTheme 복습 및 정리

--- Unit3/Pathway3/SuperHero/app/src/main/java/com/compose/camp/hun/superherocodelab/ui/theme/Shape.kt
+++ Unit3/Pathway3/SuperHero/app/src/main/java/com/compose/camp/hun/superherocodelab/ui/theme/Shape.kt
@@ -4,6 +4,13 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
+/**
+ * Shape.kt 파일
+ * Shape파일은 Compose에서 구셩요소의 도형을 정의하는데 사용된다.
+ * 구성요소에는 소형, 중형, 대형 3가지가 존재한다.
+ *
+ * Card 컴포즈함수는 기본적으로 중형 사이즈로 Shape가 설정된다.
+ */
 val Shapes = Shapes(
     small = RoundedCornerShape(8.dp),
     medium = RoundedCornerShape(16.dp),
--- Unit3/Pathway3/SuperHero/app/src/main/java/com/compose/camp/hun/superherocodelab/ui/theme/Type.kt
+++ Unit3/Pathway3/SuperHero/app/src/main/java/com/compose/camp/hun/superherocodelab/ui/theme/Type.kt
@@ -8,6 +8,13 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.compose.camp.hun.superherocodelab.R
 
+/**
+ * FontFamily에서 Font를 가져온다.
+ * font를 가져올 때, [font파일, font타입]을 설정한다. (cabin_regular, FontWeight.Normal)
+ * 이렇게 생성된 FontFamily를 아래 Typography에 각 TextStyle(h1, h2, body1...)에 맞는 font를 설정한다.
+ *
+ * 이렇게 설정된 Typography는 MaterialTheme에 설정되고, 이를 TextView의 TextStyle을 설정하면 알맞은 폰트로 설정된다.
+ */
 val Cabin = FontFamily(
     Font(R.font.cabin_regular, FontWeight.Normal),
     Font(R.font.cabin_bold, FontWeight.Bold),
