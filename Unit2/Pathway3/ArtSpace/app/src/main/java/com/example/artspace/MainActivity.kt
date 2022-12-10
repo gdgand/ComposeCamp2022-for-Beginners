72b239f7842a6ef00876471b3b96ad3ddfcfcb3a
sgtlee99 <88578263+sgtlee99@users.noreply.github.com>
2022-12-10 10:22:55 +0900

    art space

--- Unit2/Pathway3/ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ Unit2/Pathway3/ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +26,62 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ArtSpaceTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                ArtSpaceApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ArtSpaceApp() {
+    ArtInfo()
+}
+
+@Composable
+fun ArtInfo() {
+    //이미지, 제목
+    Column(
+        modifier = Modifier.padding(5.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        //이미지
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = null
+        )
+        //제목
+        Text(
+            text = "Art Space Title",
+            fontSize = 24.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+        //작품정보
+        Text(
+            text = "Information of Art (Year)",
+            fontSize = 15.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+        //버튼
+        Row(modifier = Modifier.padding(5.dp)) {
+
+            Button(onClick = { var a = 0 }) {
+                Text(text = stringResource(R.string.previous), fontSize = 24.sp)
+            }
+            Button(onClick = { var b = 0 }) {
+                Text(text = stringResource(R.string.next), fontSize = 24.sp)
+            }
+        }
+    }
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ArtSpaceTheme {
-        Greeting("Android")
+//        ArtSpaceApp()
+        ArtInfo()
     }
 }
