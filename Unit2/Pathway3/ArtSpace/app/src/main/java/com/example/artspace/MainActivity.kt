0a88722e6e87c6fb0c20f165bb247da7a91b304d
hyeseong <hyeseong7848@gmail.com>
2022-12-11 00:44:33 +0900

    Add - ArtSpace 이미지 및 텍스트 추가

--- Unit2/Pathway3/ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ Unit2/Pathway3/ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.artspace
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ArtSpaceScreen(modifier = Modifier)
                 }
             }
         }
@@ -30,14 +42,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ArtSpaceScreen(
+    modifier: Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+
+        ArtImageUI(imgId = R.drawable.apple_workplace, stringId = R.string.str_android)
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun ArtImageUI(imgId: Int, stringId: Int) {
+    Box(modifier = Modifier.border(
+        width = 5.dp,
+        color = Color.Gray,
+        shape = RectangleShape
+    )) {
+        Image(
+            modifier = Modifier
+                .padding(20.dp),
+            painter = painterResource(imgId),
+            contentDescription = stringResource(id = stringId)
+        )
+    }
+}
+
+@Composable
+fun ArtImgContent(stringId: Int) {
+
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
 fun DefaultPreview() {
     ArtSpaceTheme {
-        Greeting("Android")
+        ArtSpaceScreen(modifier = Modifier)
     }
 }
