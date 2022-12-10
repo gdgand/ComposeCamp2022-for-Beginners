d021a1a5ff34479dc97ca37bcf5a3f92da6ac296
Charlie_Moon <moon3482@cau.ac.kr>
2022-12-10 21:18:40 +0900

    화면별 테스트 코드 작성

--- Unit4/Pathway3/basic-android-kotlin-compose-training-reply-app/app/src/androidTest/java/com/example/reply/test/ReplyAppTest.kt
+++ Unit4/Pathway3/basic-android-kotlin-compose-training-reply-app/app/src/androidTest/java/com/example/reply/test/ReplyAppTest.kt
@@ -1,4 +1,53 @@
 package com.example.reply.test
 
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import com.example.reply.ui.ReplyApp
+import org.junit.Rule
+import org.junit.Test
+import com.example.reply.R
+
 class ReplyAppTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun compactDevice_verifyUsingBottomNavigation() {
+        // Set up compact window
+        composeTestRule.setContent {
+            ReplyApp(
+                windowSize = WindowWidthSizeClass.Compact
+            )
+        }
+        composeTestRule.onNodeWithTagForStringId(
+            R.string.navigation_bottom
+        ).assertExists()
+    }
+
+    @Test
+    fun mediumDevice_verifyUsingNavigationRail() {
+        composeTestRule.setContent {
+            ReplyApp(
+                windowSize = WindowWidthSizeClass.Medium
+            )
+        }
+        composeTestRule.onNodeWithTagForStringId(
+            R.string.navigation_rail
+        ).assertExists()
+    }
+
+    @Test
+    fun expandedDevice_verifyUsingNavigationDrawer() {
+        composeTestRule.setContent {
+            ReplyApp(
+                windowSize = WindowWidthSizeClass.Expanded
+            )
+        }
+        composeTestRule.onNodeWithTagForStringId(
+            R.string.navigation_drawer
+        ).assertExists()
+    }
+
 }
--- Unit4/Pathway3/basic-android-kotlin-compose-training-reply-app/app/src/main/java/com/example/reply/ui/ReplyHomeScreen.kt
+++ Unit4/Pathway3/basic-android-kotlin-compose-training-reply-app/app/src/main/java/com/example/reply/ui/ReplyHomeScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.reply.R
@@ -76,14 +77,16 @@ fun ReplyHomeScreen(
         )
     )
     if (navigationType == ReplyNavigationType.PERMANENT_NAVIGATION_DRAWER) {
+        val navigationDrawerContentDescription = stringResource(R.string.navigation_drawer)
         PermanentNavigationDrawer(
             drawerContent = {
                 NavigationDrawerContent(
                     selectedDestination = replyUiState.currentMailbox,
                     onTabPressed = onTabPressed,
-                    navigationItemContentList = navigationItemContentList
+                    navigationItemContentList = navigationItemContentList,
                 )
-            }
+            },
+            modifier = Modifier.testTag(navigationDrawerContentDescription)
         ) {
             ReplyAppContent(
                 replyUiState = replyUiState,
@@ -133,10 +136,12 @@ private fun ReplyAppContent(
     Row(modifier = modifier.fillMaxSize()) {
 
         AnimatedVisibility(visible = navigationType == ReplyNavigationType.NAVIGATION_RAIL) {
+            val navigationRailContentDescription = stringResource(R.string.navigation_rail)
             ReplyNavigationRail(
                 currentTab = replyUiState.currentMailbox,
                 navigationItemContentList = navigationItemContentList,
-                onTabPressed = onTabPressed
+                onTabPressed = onTabPressed,
+                modifier = Modifier.testTag(navigationRailContentDescription)
             )
         }
         Column(
@@ -158,10 +163,12 @@ private fun ReplyAppContent(
                 )
 
                 AnimatedVisibility(visible = navigationType == ReplyNavigationType.BOTTOM_NAVIGATION) {
+                    val bottomNavigationContentDescription = stringResource(R.string.navigation_bottom)
                     ReplyBottomNavigationBar(
                         currentTab = replyUiState.currentMailbox,
                         onTabPressed = onTabPressed,
-                        navigationItemContentList = navigationItemContentList
+                        navigationItemContentList = navigationItemContentList,
+                        modifier = Modifier.testTag(bottomNavigationContentDescription)
                     )
                 }
             }
