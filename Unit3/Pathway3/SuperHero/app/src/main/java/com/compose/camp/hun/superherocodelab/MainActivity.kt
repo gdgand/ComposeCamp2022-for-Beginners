5d6d4a875b64f2c430f17671852825cee9a42236
dale.band <dale.band@kakaocorp.com>
2022-12-10 15:28:26 +0900

    [Unit3 Pathway3] SuperHero 완성 (솔루션 코드 전)

--- Unit3/Pathway3/SuperHero/app/src/main/java/com/compose/camp/hun/superherocodelab/MainActivity.kt
+++ Unit3/Pathway3/SuperHero/app/src/main/java/com/compose/camp/hun/superherocodelab/MainActivity.kt
@@ -3,14 +3,16 @@ package com.compose.camp.hun.superherocodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -19,6 +21,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.compose.camp.hun.superherocodelab.data.HeroesRepository
 import com.compose.camp.hun.superherocodelab.model.Hero
+import com.compose.camp.hun.superherocodelab.ui.theme.Shapes
 import com.compose.camp.hun.superherocodelab.ui.theme.SuperheroesTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,12 +29,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SuperheroesTheme() {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    SuperHeroCard(hero = HeroesRepository.heroes.first())
+                Scaffold(topBar = { SuperHeroAppBar() }) {
+                    LazyColumn() {
+                        items(HeroesRepository.heroes) { hero: Hero ->
+                            SuperHeroCard(hero = hero)
+                        }
+                    }
                 }
             }
         }
@@ -47,27 +50,66 @@ fun DefaultPreview() {
 }
 
 @Composable
+fun SuperHeroAppBar(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .height(56.dp)
+            .fillMaxWidth()
+            .background(MaterialTheme.colors.background),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = stringResource(id = R.string.app_name), style = MaterialTheme.typography.h1)
+    }
+}
+
+@Composable
 fun SuperHeroCard(hero: Hero, modifier: Modifier = Modifier) {
-    Card(modifier = modifier.clip(RoundedCornerShape(16.dp))) {
-        Row(modifier = Modifier
-            .padding(16.dp)
-            .wrapContentSize(),
-            horizontalArrangement = Arrangement.Center
+    Card(
+        modifier = modifier
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .wrapContentHeight()
+            .fillMaxWidth(), elevation = 2.dp
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround
         ) {
-            Column() {
-                Text(text = stringResource(id = hero.nameRes), style = MaterialTheme.typography.h3)
-                Text(
-                    text = stringResource(id = hero.descriptionRes),
-                    style = MaterialTheme.typography.body1
-                )
-            }
-            Image(
-                painter = painterResource(id = hero.imageRes),
-                contentDescription = stringResource(id = hero.descriptionRes),
-                modifier = Modifier
-                    .size(40.dp)
-                    .clip(RoundedCornerShape(8.dp))
-            )
+            SuperHeroInformation(name = hero.nameRes, description = hero.descriptionRes)
+            Spacer(modifier = Modifier.size(16.dp))
+            SuperHeroIcon(heroIcon = hero.imageRes, contentDescription = hero.descriptionRes)
         }
     }
+}
+
+@Composable
+fun SuperHeroInformation(
+    @StringRes name: Int,
+    @StringRes description: Int,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier) {
+        Text(text = stringResource(id = name), style = MaterialTheme.typography.h3)
+        Text(
+            text = stringResource(id = description),
+            style = MaterialTheme.typography.body1,
+            maxLines = 2
+        )
+    }
+}
+
+@Composable
+fun SuperHeroIcon(
+    @DrawableRes heroIcon: Int,
+    @StringRes contentDescription: Int?,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        painter = painterResource(id = heroIcon),
+        contentDescription = contentDescription?.let { stringResource(id = it) },
+        modifier = modifier
+            .clip(Shapes.small),
+        alignment = Alignment.CenterEnd
+    )
 }
