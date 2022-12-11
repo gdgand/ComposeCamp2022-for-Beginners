8a7115b377ca32160c6a62b033774fc54d85bd8e
JPark <yellowj83@gmail.com>
2022-12-11 20:18:33 +0900

    ArtSpace 작품 이동 기능 추가

--- Unit2/Pathway3/ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ Unit2/Pathway3/ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -9,9 +9,10 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,39 +38,92 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Composable
 fun ArtSpaceScreen() {
+
+    var index by remember {
+        mutableStateOf(0)
+    }
+
+    val image = when (index) {
+        0 -> painterResource(id = R.drawable.image_1)
+        1 -> painterResource(id = R.drawable.image_2)
+        2 -> painterResource(id = R.drawable.image_3)
+        else -> painterResource(id = R.drawable.image_1)
+    }
+
+    val contentDesc = when (index) {
+        0 -> "buldak"
+        1 -> "abon"
+        2 -> "redmage"
+        else -> "buldak"
+    }
+
+    val title = when (index) {
+        0 -> "Buldak"
+        1 -> "Abon"
+        2 -> "Red Mage"
+        else -> "Buldak"
+    }
+
+    val artist = when (index) {
+        0 -> "NEXON"
+        1 -> "NEXON"
+        2 -> "SQUARE ENIX"
+        else -> "NEXON"
+    }
+
     Column(modifier = Modifier
         .fillMaxSize()
         .wrapContentSize(Alignment.Center), horizontalAlignment = Alignment.CenterHorizontally) {
-        Image(painter = painterResource(id = R.drawable.image_1), contentDescription = "")
+        ArtArea(image, contentDesc, title, artist)
         Spacer(modifier = Modifier.height(10.dp))
-        ArtTitleArea()
-        Spacer(modifier = Modifier.height(10.dp))
-        ControlButtonArea()
+        ControlButtonArea({
+            index = when (index) {
+                0 -> 2
+                else -> index - 1
+        } }) {
+            index = when (index) {
+                2 -> 0
+                else -> index + 1
+            }
+        }
+        Text(text = index.toString())
     }
 }
 
 @Composable
-fun ArtTitleArea() {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Artwork Title", fontSize = 24.sp, fontWeight = FontWeight.Bold)
-        Text(text = "Artwork Artist (Year)")
+fun ArtArea(
+    image : Painter,
+    contentDescription : String,
+    title: String,
+    artist: String
+) {
+    Column() {
+        Image(painter = image, contentDescription = contentDescription)
+        Spacer(modifier = Modifier.height(10.dp))
+        ArtTitleArea(title, artist)
     }
 }
 
 @Composable
-fun ControlButtonArea() {
+fun ArtTitleArea(title : String, artist : String) {
+    Column(horizontalAlignment = Alignment.Start) {
+        Text(text = title, fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        Text(text = artist)
+    }
+}
+
+@Composable
+fun ControlButtonArea(
+    preClick: () -> Unit,
+    nextClick: () -> Unit
+) {
     Row() {
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = preClick) {
             Text(text = "Previous")
         }
         Spacer(modifier = Modifier.width(10.dp))
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = nextClick) {
             Text(text = "Next")
         }
     }
