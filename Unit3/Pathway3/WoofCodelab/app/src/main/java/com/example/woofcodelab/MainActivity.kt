c902da5b9649f05fb81710873798a3013a877ffd
doubleclutch30 <cometruth30@gmail.com>
2022-12-11 22:06:34 +0900

    Unit3 - Woof App Dog 이미지 원형으로 표시

--- Unit3/Pathway3/WoofCodelab/app/src/main/java/com/example/woofcodelab/MainActivity.kt
+++ Unit3/Pathway3/WoofCodelab/app/src/main/java/com/example/woofcodelab/MainActivity.kt
@@ -29,10 +29,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -94,7 +97,9 @@ fun DogIcon(@DrawableRes dogIcon: Int, modifier: Modifier = Modifier) {
     Image(
         modifier = modifier
             .size(64.dp)
-            .padding(8.dp),
+            .padding(8.dp)
+            .clip(RoundedCornerShape(50)),
+        contentScale = ContentScale.Crop,
         painter = painterResource(dogIcon),
         /*
          * Content Description is not needed here - image is decorative, and setting a null content
