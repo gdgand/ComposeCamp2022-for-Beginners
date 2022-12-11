0291ca559e1a499518a2ccfb83f8a8a1c3b8a633
shy1008 <lokixa96@gmail.com>
2022-12-11 13:15:15 +0900

    ArtSpace codelab

---
+++ Unit2/Pathway3/ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -0,0 +1,178 @@
+package com.example.artspace
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.artspace.ui.theme.ArtSpaceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ArtSpaceTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    ArtSpaceScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ArtSpaceScreen() {
+    var state by remember { mutableStateOf(1) }
+
+    Column(
+        modifier = Modifier.padding(24.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        ImageAndInfo(state)
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Button(onClick = { state-- } ) {
+                Text(stringResource(R.string.previous))
+            }
+            Button(onClick = { state++ } ) {
+                Text(stringResource(R.string.next))
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageAndInfo(
+    state: Int
+){
+
+    @DrawableRes var imageId: Int = R.drawable.img1
+    @StringRes var imageDescription: Int = R.string.img_desc_1
+    @StringRes var title: Int = R.string.title_1
+    @StringRes var detail: Int = R.string.detail_1
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        when(state) {
+            1 -> {
+                imageId = R.drawable.img1
+                imageDescription = R.string.img_desc_1
+                title = R.string.title_1
+                detail = R.string.detail_1
+            }
+            2 -> {
+                imageId = R.drawable.img2
+                imageDescription = R.string.img_desc_2
+                title = R.string.title_2
+                detail = R.string.detail_2
+            }
+            3 -> {
+                imageId = R.drawable.img3
+                imageDescription = R.string.img_desc_3
+                title = R.string.title_3
+                detail = R.string.detail_3
+            }
+            4 -> {
+                imageId = R.drawable.img4
+                imageDescription = R.string.img_desc_4
+                title = R.string.title_4
+                detail = R.string.detail_4
+            }
+            5 -> {
+                imageId = R.drawable.img5
+                imageDescription = R.string.img_desc_5
+                title = R.string.title_5
+                detail = R.string.detail_5
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .border(width = 1.dp, color = Color.Black)
+        ) {
+            val configuration = LocalConfiguration.current
+            val screenWidth = configuration.screenWidthDp.dp
+
+            if (screenWidth > 800.dp) {
+                Image(
+                    painter = painterResource(id = imageId),
+                    contentDescription = stringResource(id = imageDescription),
+                    modifier = Modifier.padding(30.dp)
+                        .fillMaxHeight(0.7F),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            else{
+                Image(
+                    painter = painterResource(id = imageId),
+                    contentDescription = stringResource(id = imageDescription),
+                    modifier = Modifier.padding(30.dp)
+                        .fillMaxWidth()
+                        .fillMaxHeight(0.7F),
+                    contentScale = ContentScale.Crop
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .border(
+                    width = 1.dp,
+                    color = Color.Companion.Gray,
+                    shape = RoundedCornerShape(10.dp)
+                )
+        ) {
+            Text(
+                text = stringResource(id = title),
+                fontSize = 36.sp,
+                modifier = Modifier.padding(25.dp, 10.dp, 25.dp, 5.dp),
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1
+            )
+            Text(
+                text = stringResource(id = detail),
+                modifier = Modifier.padding(25.dp, 5.dp, 25.dp, 10.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    ArtSpaceTheme {
+        ArtSpaceScreen()
+    }
+}
