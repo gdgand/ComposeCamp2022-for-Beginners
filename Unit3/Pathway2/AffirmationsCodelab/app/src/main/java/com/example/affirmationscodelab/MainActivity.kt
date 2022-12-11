20f60c0d644d9403a5cb891be45168d7adcdfda2
Sujin Kim <sujinkim919@gmail.com>
2022-12-11 21:08:46 +0900

    feat(Unit3/Pathway2/AffirmationsCodelab): add card list

--- Unit3/Pathway2/AffirmationsCodelab/app/src/main/java/com/example/affirmationscodelab/MainActivity.kt
+++ Unit3/Pathway2/AffirmationsCodelab/app/src/main/java/com/example/affirmationscodelab/MainActivity.kt
@@ -15,6 +15,7 @@
  */
 package com.example.affirmationscodelab
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -23,6 +24,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -35,6 +38,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.affirmationscodelab.data.Datasource
 import com.example.affirmationscodelab.model.Affirmation
 import com.example.affirmationscodelab.ui.theme.AffirmationsTheme
 
@@ -52,6 +56,19 @@ fun AffirmationApp() {
     val context = LocalContext.current
 
     AffirmationsTheme {
+        AffirmationList(affirmationList = Datasource().loadAffirmations())
+    }
+}
+
+@Composable
+fun AffirmationList(
+    affirmationList: List<Affirmation>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn {
+        items(affirmationList) { affirmation ->
+            AffirmationCard(affirmation = affirmation)
+        }
     }
 }
 
