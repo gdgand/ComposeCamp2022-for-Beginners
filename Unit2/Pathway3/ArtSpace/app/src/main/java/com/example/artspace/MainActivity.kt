1b74a160769aa18ea8130a1ebccd780bcccd65b7
hyeseong <hyeseong7848@gmail.com>
2022-12-12 22:29:26 +0900

    Add - ArtSpace 기능 추가

--- Unit2/Pathway3/ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ Unit2/Pathway3/ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.example.artspace
 
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,10 +18,20 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
+private val itemList = listOf(
+    ArtItem(R.drawable.android, R.string.str_android, R.string.str_android_by),
+    ArtItem(R.drawable.apple_workplace, R.string.str_apple_workplace, R.string.str_apple_workplace_by),
+    ArtItem(R.drawable.comet, R.string.str_comet, R.string.str_comet_by),
+    ArtItem(R.drawable.sea, R.string.str_sea, R.string.str_sea_by),
+    ArtItem(R.drawable.space, R.string.str_space, R.string.str_space_by),
+    ArtItem(R.drawable.night_sky, R.string.str_night_sky, R.string.str_night_sky_by),
+    ArtItem(R.drawable.mountain, R.string.str_mountain, R.string.str_mountain_by)
+)
+
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -34,7 +41,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    ArtSpaceScreen(modifier = Modifier)
+                    ArtSpaceScreen()
                 }
             }
         }
@@ -42,37 +49,91 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ArtSpaceScreen(
-    modifier: Modifier
+private fun ArtSpaceScreen(
 ) {
+    var step by remember { mutableStateOf(0) }
+    val artWork: ArtItem = itemList[step]
+
+    val imageId = artWork.imgId
+    val titleId = artWork.titleId
+    val discripId = artWork.discripId
+
     Column(
-        modifier = modifier
+        modifier = Modifier
+            .padding(start = 16.dp, end = 16.dp)
+            .fillMaxSize(),
     ) {
-
-        ArtImageUI(imgId = R.drawable.apple_workplace, stringId = R.string.str_android)
-    }
-}
-
-@Composable
-fun ArtImageUI(imgId: Int, stringId: Int) {
-    Box(modifier = Modifier.border(
-        width = 5.dp,
-        color = Color.Gray,
-        shape = RectangleShape
-    )) {
-        Image(
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
             modifier = Modifier
-                .padding(20.dp),
-            painter = painterResource(imgId),
-            contentDescription = stringResource(id = stringId)
-        )
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            Card(
+                shape = RectangleShape,
+                border = BorderStroke(5.dp, Color.Gray),
+                elevation = 5.dp
+            ) {
+                Image(
+                    painter = painterResource(id = imageId),
+                    contentDescription = stringResource(id = titleId),
+                    modifier = Modifier.padding(20.dp)
+                )
+            }
+        }
+        Card(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            elevation = 8.dp
+        ) {
+            Column(
+                horizontalAlignment = Alignment.Start,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(
+                    text = stringResource(id = titleId),
+                    style = MaterialTheme.typography.h5
+                )
+                Text(
+                    text = stringResource(id = discripId)
+                )
+            }
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Button(
+                onClick = {
+                    if (step == 0) {
+                        step = itemList.size - 1
+                    } else {
+                        step -= 1
+                    }
+                },
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(text = stringResource(id = R.string.str_previous))
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Button(
+                onClick = {
+                    if (step == itemList.size - 1) step = 0
+                    else step++
+                },
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(text = stringResource(id = R.string.str_next))
+            }
+        }
     }
 }
 
-@Composable
-fun ArtImgContent(stringId: Int) {
 
-}
 
 @Preview(
     showBackground = true,
@@ -81,6 +142,13 @@ fun ArtImgContent(stringId: Int) {
 @Composable
 fun DefaultPreview() {
     ArtSpaceTheme {
-        ArtSpaceScreen(modifier = Modifier)
+        ArtSpaceScreen()
     }
 }
+
+
+data class ArtItem(
+    @DrawableRes val imgId: Int,
+    @StringRes val titleId: Int,
+    @StringRes val discripId: Int
+)
