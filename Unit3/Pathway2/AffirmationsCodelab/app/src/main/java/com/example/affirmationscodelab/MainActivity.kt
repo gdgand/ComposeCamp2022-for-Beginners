9a1b8d6b10cc6d541614694f83fbcf6465b020da
hyeseong <hyeseong7848@gmail.com>
2022-12-12 22:46:51 +0900

    Add - AffirmationsCodelab 추가중

--- Unit3/Pathway2/AffirmationsCodelab/app/src/main/java/com/example/affirmationscodelab/MainActivity.kt
+++ Unit3/Pathway2/AffirmationsCodelab/app/src/main/java/com/example/affirmationscodelab/MainActivity.kt
@@ -14,11 +14,27 @@
  * limitations under the License.
  */
 package com.example.affirmationscodelab
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.affirmationscodelab.model.Affirmation
 import com.example.affirmationscodelab.ui.theme.AffirmationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +48,40 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AffirmationApp() {
+    val context = LocalContext.current
     AffirmationsTheme {
+        AffirmationCard(Affirmation(R.string.affirmation1, R.drawable.image1))
+    }
+}
+
+@Composable
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
+    Card(modifier = modifier.padding(8.dp), elevation = 4.dp) {
+        Column {
+            Image(
+                painter = painterResource(affirmation.imageResourceId),
+                contentDescription = stringResource(affirmation.stringResourceId),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = stringResource(affirmation.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.h6
+            )
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun DefaultPreview() {
+    AffirmationsTheme {
+        AffirmationApp()
     }
 }
