e2441914c911dc9222db6abc6bfa4d11acccc5c4
april <aprilgom@gmail.com>
2022-12-13 19:47:51 +0900

    Guess word check

--- Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
+++ Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
@@ -8,6 +8,7 @@ import com.example.android.unscramble.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 
 class GameViewModel : ViewModel(){
@@ -42,6 +43,16 @@ class GameViewModel : ViewModel(){
     }
     fun updateUserGuess(guessedWord: String) {
         userGuess = guessedWord
+    }
+    fun checkUserGuess() {
+        if (userGuess.equals(currentWord, ignoreCase = true)) {
+
+        } else {
+            _uiState.update { currentState ->
+                currentState.copy(isGuessedWordWrong = true)
+            }
+        }
+        updateUserGuess("")
     }
     init {
         resetGame()
