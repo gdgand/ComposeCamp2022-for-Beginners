0175cf3cc1084a086619b788d016ae238a637088
april <aprilgom@gmail.com>
2022-12-13 19:28:13 +0900

    view model design

--- Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
+++ Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
@@ -1,17 +1,23 @@
 package com.example.android.unscramble.ui
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.android.unscramble.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-private val _uiState = MutableStateFlow(GameUiState())
-val uiState:  StateFlow<GameUiState> = _uiState.asStateFlow()
 
 class GameViewModel : ViewModel(){
+    private val _uiState = MutableStateFlow(GameUiState())
+    val uiState:  StateFlow<GameUiState> = _uiState.asStateFlow()
+
     private lateinit var currentWord:String
     private var usedWords: MutableSet<String> = mutableSetOf()
+    var userGuess by mutableStateOf("")
+        private set
 
     private fun pickRandomWordAndShuffle() : String{
         currentWord = allWords.random()
@@ -33,6 +39,9 @@ class GameViewModel : ViewModel(){
     fun resetGame() {
         usedWords.clear()
         _uiState.value = GameUiState(currentScrambleWord = pickRandomWordAndShuffle())
+    }
+    fun updateUserGuess(guessedWord: String) {
+        userGuess = guessedWord
     }
     init {
         resetGame()
