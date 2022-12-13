12f646005de4b0b2d71c3c3b760adcb0314aa704
april <aprilgom@gmail.com>
2022-12-13 16:38:10 +0900

    topappbar

--- Unit3/Pathway3/WoofCodelab/app/src/main/java/com/example/woofcodelab/MainActivity.kt
+++ Unit3/Pathway3/WoofCodelab/app/src/main/java/com/example/woofcodelab/MainActivity.kt
@@ -15,6 +15,7 @@
  */
 package com.example.woofcodelab
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -32,8 +33,10 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -61,12 +64,39 @@ class MainActivity : ComponentActivity() {
  */
 @Composable
 fun WoofApp() {
-    LazyColumn (modifier = Modifier
-        .background(MaterialTheme.colors.background)
-    ){
-        items(dogs) {
-            DogItem(dog = it)
+    Scaffold(
+        topBar = {
+            WoofTopAppBar()
         }
+    ) { padding ->
+        LazyColumn (modifier = Modifier
+            .background(MaterialTheme.colors.background)
+            .padding(padding)
+        ){
+            items(dogs) {
+                DogItem(dog = it)
+            }
+        }
+    }
+}
+@Composable
+fun WoofTopAppBar(modifier: Modifier = Modifier) {
+    Row(modifier = modifier
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colors.primary),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            modifier = Modifier
+                .size(64.dp)
+                .padding(8.dp),
+            painter = painterResource(id = R.drawable.ic_woof_logo),
+            contentDescription = null,
+        )
+        Text(
+            text = stringResource(id = R.string.app_name),
+            style = MaterialTheme.typography.h1
+        )
     }
 }
 
