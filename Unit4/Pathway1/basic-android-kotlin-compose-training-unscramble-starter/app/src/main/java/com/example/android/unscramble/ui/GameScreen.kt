03a4cf7d5e498165760eb11296a4e296c7ea69b9
april <aprilgom@gmail.com>
2022-12-13 19:04:48 +0900

    view model added

---
+++ Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/android/unscramble/ui/GameScreen.kt
@@ -0,0 +1,187 @@
+/*
+ * Copyright (c)2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.unscramble.ui
+
+import android.app.Activity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.android.unscramble.R
+import com.example.android.unscramble.ui.theme.UnscrambleTheme
+
+
+@Composable
+fun GameScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+
+        GameStatus()
+        GameLayout()
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            OutlinedButton(
+                onClick = { },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 8.dp)
+            ) {
+                Text(stringResource(R.string.skip))
+            }
+
+            Button(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .padding(start = 8.dp),
+                onClick = { }
+            ) {
+                Text(stringResource(R.string.submit))
+            }
+        }
+    }
+}
+
+@Composable
+fun GameStatus(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .size(48.dp),
+    ) {
+        Text(
+            text = stringResource(R.string.word_count, 0),
+            fontSize = 18.sp,
+        )
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End),
+            text = stringResource(R.string.score, 0),
+            fontSize = 18.sp,
+        )
+    }
+}
+
+@Composable
+fun GameLayout(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(24.dp),
+
+        ) {
+        Text(
+            text = "scrambleun",
+            fontSize = 45.sp,
+            modifier = modifier.align(Alignment.CenterHorizontally)
+        )
+        Text(
+            text = stringResource(R.string.instructions),
+            fontSize = 17.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+        OutlinedTextField(
+            value = "",
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            onValueChange = { },
+            label = { Text(stringResource(R.string.enter_your_word)) },
+            isError = false,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = { }
+            ),
+        )
+    }
+}
+
+/*
+ * Creates and shows an AlertDialog with final score.
+ */
+@Composable
+private fun FinalScoreDialog(
+    onPlayAgain: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val activity = (LocalContext.current as Activity)
+
+    AlertDialog(
+        onDismissRequest = {
+            // Dismiss the dialog when the user clicks outside the dialog or on the back
+            // button. If you want to disable that functionality, simply use an empty
+            // onCloseRequest.
+        },
+        title = { Text(stringResource(R.string.congratulations)) },
+        text = { Text(stringResource(R.string.you_scored, 0)) },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    activity.finish()
+                }
+            ) {
+                Text(text = stringResource(R.string.exit))
+            }
+        },
+        confirmButton = {
+            TextButton(
+                onClick = onPlayAgain
+            ) {
+                Text(text = stringResource(R.string.play_again))
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    UnscrambleTheme {
+        GameScreen()
+    }
+}
