afafaeb798c887938e14894c91782db9b678230e
dehan <dehan27@gmail.com>
2022-12-14 02:38:12 +0900

    Unit4 Pathway2 Cupcake(Navigation Training) Complete

--- Unit4/Pathway2/basic-android-kotlin-compose-training-cupcake/app/src/main/java/com/example/cupcake/ui/StartOrderScreen.kt
+++ Unit4/Pathway2/basic-android-kotlin-compose-training-cupcake/app/src/main/java/com/example/cupcake/ui/StartOrderScreen.kt
@@ -45,7 +45,7 @@ import com.example.cupcake.data.DataSource.quantityOptions
 @Composable
 fun StartOrderScreen(
     quantityOptions: List<Pair<Int, Int>>,
-    // TODO: add onNextButtonClicked
+    onNextButtonClicked: (Int) -> Unit,
     modifier: Modifier = Modifier
 ){
     Column(
@@ -65,7 +65,7 @@ fun StartOrderScreen(
         quantityOptions.forEach { item ->
             SelectQuantityButton(
                 labelResourceId = item.first,
-                onClick = { /* TODO: handle next button */ }
+                onClick = { onNextButtonClicked(item.second) }
             )
         }
     }
@@ -92,5 +92,8 @@ fun SelectQuantityButton(
 @Preview
 @Composable
 fun StartOrderPreview(){
-    StartOrderScreen(quantityOptions = quantityOptions)
+    StartOrderScreen(
+        quantityOptions = quantityOptions,
+        onNextButtonClicked = {}
+    )
 }
