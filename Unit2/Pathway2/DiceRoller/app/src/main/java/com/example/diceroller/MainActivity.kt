6d936c6bb69cc5588acdd83a782878d421301828
김상익 <ik741953@gmail.com>
2022-12-13 16:01:48 +0900

    wrapContentSize() 메서드를 Modifier 객체에 체이닝하고 Alignment.Center를 인수로 전달하여 구성요소를 중앙에 배치합. * Alignment.Center는 구성요소가 세로와 가로로 모두 중앙에 배치되도록 지정합니다. *

--- Unit2/Pathway2/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Unit2/Pathway2/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroller.ui.theme.DiceRollerTheme
@@ -31,6 +33,8 @@ fun DiceRollerApp() {
 
 @Composable
 fun DiceWithButtonAndImage(modifier:Modifier = Modifier
-    .fillMaxSize()) {
+    .fillMaxSize()
+    .wrapContentSize(Alignment.Center)
+) {
 
 }
