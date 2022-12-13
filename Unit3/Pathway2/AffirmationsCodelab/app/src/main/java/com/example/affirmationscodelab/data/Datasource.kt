ba86e3ee0236bc81abbe0b75e29a7287c33662cd
kkrnl <krnl3321@gmail.com>
2022-12-13 09:43:25 +0900

    Unit 3 Affirmations

--- Unit3/Pathway2/AffirmationsCodelab/app/src/main/java/com/example/affirmationscodelab/data/Datasource.kt
+++ Unit3/Pathway2/AffirmationsCodelab/app/src/main/java/com/example/affirmationscodelab/data/Datasource.kt
@@ -26,7 +26,7 @@ class Datasource() {
             Affirmation(R.string.affirmation1, R.drawable.image1),
             Affirmation(R.string.affirmation2, R.drawable.image2),
             Affirmation(R.string.affirmation3, R.drawable.image3),
-            Affirmation(R.string.affirmation4, R.drawable.image4),
+            Affirmation(R.string.affirmation4, R.drawable.image1),
             Affirmation(R.string.affirmation5, R.drawable.image5),
             Affirmation(R.string.affirmation6, R.drawable.image6),
             Affirmation(R.string.affirmation7, R.drawable.image7),
