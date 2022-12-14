9b3b550e122a6463b98490c13d9871d9f7044422
qkrdygks3023 <55884525+qkrdygks3023@users.noreply.github.com>
2022-12-15 03:31:27 +0900

    Pathway3

--- Unit3/Pathway3/WoofCodelab/app/src/main/java/com/example/woofcodelab/MainActivity.kt
+++ Unit3/Pathway3/WoofCodelab/app/src/main/java/com/example/woofcodelab/MainActivity.kt
@@ -21,16 +21,20 @@ import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,6 +42,7 @@ import androidx.compose.ui.unit.dp
 import com.example.woofcodelab.data.Dog
 import com.example.woofcodelab.data.dogs
 import com.example.woofcodelab.ui.theme.WoofTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,11 +60,25 @@ class MainActivity : ComponentActivity() {
  */
 @Composable
 fun WoofApp() {
-    LazyColumn {
-        items(dogs) {
-            DogItem(dog = it)
-        }
-    }
+    Scaffold(
+        topBar = {
+            WoofTopAppBar()
+        },
+                content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(it)
+            ) {
+                LazyColumn(modifier = Modifier.background(MaterialTheme.colors.background)) {
+                    items(dogs) {
+                        DogItem(dog = it)
+                    }
+                }
+            }}
+
+
+    )
 }
 
 /**
@@ -70,14 +89,18 @@ fun WoofApp() {
  */
 @Composable
 fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp)
-    ) {
-        DogIcon(dog.imageResourceId)
-        DogInformation(dog.name, dog.age)
-    }
+   Card( modifier = Modifier.padding(8.dp),
+       elevation = 4.dp) {
+       Row(
+           modifier = Modifier
+               .fillMaxWidth()
+               .padding(8.dp)
+
+       ) {
+           DogIcon(dog.imageResourceId)
+           DogInformation(dog.name, dog.age)
+       }
+   }
 }
 
 /**
@@ -91,7 +114,11 @@ fun DogIcon(@DrawableRes dogIcon: Int, modifier: Modifier = Modifier) {
     Image(
         modifier = modifier
             .size(64.dp)
-            .padding(8.dp),
+            .padding(8.dp)
+            .clip(
+                RoundedCornerShape(50)
+            ),
+        contentScale = ContentScale.Crop,
         painter = painterResource(dogIcon),
         /*
          * Content Description is not needed here - image is decorative, and setting a null content
@@ -113,10 +140,39 @@ fun DogInformation(@StringRes dogName: Int, dogAge: Int, modifier: Modifier = Mo
     Column {
         Text(
             text = stringResource(dogName),
-            modifier = modifier.padding(top = 8.dp)
+            style = MaterialTheme.typography.h2,
+                modifier = modifier.padding(top = 8.dp)
         )
         Text(
-            text = stringResource(R.string.years_old, dogAge)
+            text = stringResource(R.string.years_old, dogAge),
+                    style = MaterialTheme.typography.body1
+        )
+    }
+}
+
+@Composable
+fun WoofTopAppBar(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colors.primary),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            modifier = modifier
+                .size(64.dp)
+                .padding(8.dp),
+            painter = painterResource(R.drawable.ic_woof_logo),
+            /*
+             * Content Description is not needed here - image is decorative, and setting a null
+             * content description allows accessibility services to skip this element during
+             * navigation.
+             */
+            contentDescription = null
+        )
+        Text(
+            text = stringResource(R.string.app_name),
+            style = MaterialTheme.typography.h1
         )
     }
 }
@@ -127,7 +183,7 @@ fun DogInformation(@StringRes dogName: Int, dogAge: Int, modifier: Modifier = Mo
 @Preview
 @Composable
 fun WoofPreview() {
-    WoofTheme(darkTheme = false) {
+    WoofTheme(darkTheme = true) {
         WoofApp()
     }
 }
--- Unit3/Pathway3/WoofCodelab/app/src/main/java/com/example/woofcodelab/ui/theme/Theme.kt
+++ Unit3/Pathway3/WoofCodelab/app/src/main/java/com/example/woofcodelab/ui/theme/Theme.kt
@@ -16,22 +16,46 @@
 package com.example.woofcodelab.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+val Grey50 = Color(0xFFF8F9FA)
+val Grey900 = Color(0xFF202124)
+val Grey700 = Color(0xFF5F6368)
+val Green50 = Color(0xFFE6F4EA)
+val Green100 = Color(0xFFCEEAD6)
+
+//Dark Theme
+val White = Color(0xFFFFFFFF)
+val Grey100 = Color(0xFFF1F3F4)
+val Cyan900 = Color(0xFF007B83)
+val Cyan700 = Color(0xFF129EAF)
+
+
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    background = Cyan900,
+    surface = Cyan700,
+    onSurface = White,
+    primary = Grey900,
+    onPrimary = White,
+    secondary = Grey100
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
 
+private val LightColorPalette = lightColors(
+    background = Green100,
+    surface = Green50,
+    onSurface = Grey900,
+    primary = Grey50,
+    onPrimary = Grey900,
+    secondary = Grey700
     /* Other default colors to override
     background = Color.White,
     surface = Color.White,
@@ -49,6 +73,12 @@ fun WoofTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable (
     } else {
         LightColorPalette
     }
+
+    val Shapes = Shapes(
+        small = RoundedCornerShape(4.dp),
+        medium = RoundedCornerShape(16.dp),
+        large = RoundedCornerShape(0.dp)
+    )
 
     MaterialTheme(
         colors = colors,
