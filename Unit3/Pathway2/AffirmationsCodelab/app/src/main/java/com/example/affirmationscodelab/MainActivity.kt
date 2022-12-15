d93ae47f3d0a026f014bb2c0d5e36975f215fcf0
ss99x2002 <ss99x200211@gmail.com>
2022-12-15 17:03:51 +0900

    [ADD] Affirmation 화면 추가

--- Unit3/Pathway2/AffirmationsCodelab/app/src/main/java/com/example/affirmationscodelab/MainActivity.kt
+++ Unit3/Pathway2/AffirmationsCodelab/app/src/main/java/com/example/affirmationscodelab/MainActivity.kt
@@ -17,8 +17,23 @@ package com.example.affirmationscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.affirmationscodelab.data.Datasource
+import com.example.affirmationscodelab.model.Affirmation
 import com.example.affirmationscodelab.ui.theme.AffirmationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,9 +44,40 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+@Composable
+fun AffirmationCard(affirmation: Affirmation,modifier: Modifier = Modifier) {
+    Column() {
+        Image(
+            painter = painterResource(id = affirmation.imageResourceId),
+            contentDescription = stringResource(
+                id = affirmation.stringResourceId
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(194.dp),
+            contentScale = ContentScale.Crop
+        )
+            Text(
+                text = stringResource(id = affirmation.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.h6
+            )
+    }
+}
 
+@Composable
+private fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
+    Column() {
+        affirmationList.forEach {
+            affirmation -> AffirmationCard(affirmation = affirmation) 
+        }
+    }
+}
+
+@Preview(showBackground = true)
 @Composable
 fun AffirmationApp() {
     AffirmationsTheme {
+     AffirmationList(affirmationList = Datasource().loadAffirmations())
     }
 }
