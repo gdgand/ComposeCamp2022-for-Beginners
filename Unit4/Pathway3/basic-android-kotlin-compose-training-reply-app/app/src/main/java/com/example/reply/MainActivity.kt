11e8fa08ee56569c1e4cf1a819fe10b91eafeb74
wkdqhrud4870 <jangbg4870@gmail.com>
2022-12-15 14:11:07 +0900

    학습_COMPOSE 사용 시 알아야 하는 ANDROID 기본사항_NAVIGATION AND ARCHITECTURE_ADAPTIVE LAYOUTS_3. 동적 탐색으로 적응형 앱 빌드

--- Unit4/Pathway3/basic-android-kotlin-compose-training-reply-app/app/src/main/java/com/example/reply/MainActivity.kt
+++ Unit4/Pathway3/basic-android-kotlin-compose-training-reply-app/app/src/main/java/com/example/reply/MainActivity.kt
@@ -19,6 +19,9 @@ package com.example.reply
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.reply.ui.ReplyApp
@@ -28,12 +31,16 @@ import com.example.reply.ui.theme.ReplyTheme
  * Activity for Reply app
  */
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             ReplyTheme {
-                ReplyApp()
+                val windowSize = calculateWindowSizeClass(this)
+                ReplyApp(
+                    windowSize = windowSize.widthSizeClass
+                )
             }
         }
     }
@@ -43,6 +50,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ReplyAppPreview() {
     ReplyTheme {
-        ReplyApp()
+        ReplyApp(
+            windowSize = WindowWidthSizeClass.Compact
+        )
     }
 }
