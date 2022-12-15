3c1319a8139356818a841f6460a2b3ec544d7b78
seungwon <ysw8160@naver.com>
2022-12-15 13:37:28 +0900

    TipTime - 작업

---
+++ Unit2/Pathway3/TipTime/app/src/main/java/com/konai/tiptime/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.konai.tiptime
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.konai.tiptime.ui.theme.TipTimeTheme
+
+class MainActivity : ComponentActivity() {
+    private var amountInput = mutableStateOf("0")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TipTimeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TipTimeScreen()
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun EditNumberField() {
+        TextField(value = amountInput.value, onValueChange = {})
+    }
+
+    @Composable
+    fun TipTimeScreen() {
+        Column(modifier = Modifier.padding(32.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
+            Text(text = stringResource(id = R.string.calculate_tip), fontSize = 24.sp, modifier = Modifier.align(Alignment.CenterHorizontally))
+            Spacer(modifier = Modifier.height(16.dp))
+            EditNumberField()
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        TipTimeTheme {
+            TipTimeScreen()
+        }
+    }
+}
