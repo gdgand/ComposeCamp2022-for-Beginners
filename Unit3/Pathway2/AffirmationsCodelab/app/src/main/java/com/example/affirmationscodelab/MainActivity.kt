4451da51f28f7adc7fa4ec0b826cb4be83acc72a
Yuhee Kim <dbgml4509@naver.com>
2022-12-16 18:43:18 +0900

    feat : 사진, 텍스트 단일 레이아웃 생성

--- Unit3/Pathway2/AffirmationsCodelab/app/src/main/java/com/example/affirmationscodelab/MainActivity.kt
+++ Unit3/Pathway2/AffirmationsCodelab/app/src/main/java/com/example/affirmationscodelab/MainActivity.kt
@@ -17,8 +17,23 @@ package com.example.affirmationscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.affirmationscodelab.model.Affirmation
 import com.example.affirmationscodelab.ui.theme.AffirmationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,6 +42,33 @@ class MainActivity : ComponentActivity() {
         setContent {
             AffirmationApp()
         }
+    }
+}
+
+@Composable
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier){
+    Column {
+        Image(
+            painter = painterResource(id = affirmation.imageResourceId),
+            contentDescription = stringResource(id = affirmation.stringResourceId),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(194.dp),
+            contentScale = ContentScale.Crop
+        )
+        Text(
+            text = stringResource(id = affirmation.stringResourceId),
+            modifier = Modifier.padding(16.dp),
+            style = MaterialTheme.typography.h6
+        )
+    }
+}
+
+@Preview
+@Composable
+fun AffirmationCardPreview(){
+    AffirmationsTheme {
+        AffirmationCard(affirmation = Affirmation(R.string.affirmation1, R.drawable.image1))
     }
 }
 
