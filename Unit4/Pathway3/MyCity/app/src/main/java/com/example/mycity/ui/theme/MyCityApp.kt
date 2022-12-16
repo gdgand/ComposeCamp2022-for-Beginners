55b0413d7290a77cc9583a809f8d2a12618ffe6d
Atlas <potatoartie@gmail.com>
2022-12-16 21:54:23 +0900

    [add] mycity

---
+++ Unit4/Pathway3/MyCity/app/src/main/java/com/example/mycity/ui/theme/MyCityApp.kt
@@ -0,0 +1,54 @@
+package com.example.mycity.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.window.core.layout.WindowWidthSizeClass
+
+
+enum class MyCityNavigationType {
+    BOTTOM_NAVIGATION, NAVIGATION_RAIL, PERMANENT_NAVIGATION_DRAWER
+}
+
+
+@Composable
+fun MyCityApp(
+    windowSize: WindowWidthSizeClass,
+    modifier: Modifier = Modifier
+) {
+    val navigationType: MyCityNavigationType
+    val viewModel: ReplyViewModel = viewModel()
+    val replyUiState = viewModel.uiState.collectAsState().value
+
+    when (windowSize) {
+        WindowWidthSizeClass.Compact -> {
+            navigationType = ReplyNavigationType.BOTTOM_NAVIGATION
+        }
+        WindowWidthSizeClass.Medium -> {
+            navigationType = ReplyNavigationType.NAVIGATION_RAIL
+        }
+        WindowWidthSizeClass.Expanded -> {
+            navigationType = ReplyNavigationType.PERMANENT_NAVIGATION_DRAWER
+        }
+        else -> {
+            navigationType = ReplyNavigationType.BOTTOM_NAVIGATION
+        }
+    }
+
+    ReplyHomeScreen(
+        navigationType = navigationType,
+        replyUiState = replyUiState,
+        onTabPressed = { mailboxType: MailboxType ->
+            viewModel.updateCurrentMailbox(mailboxType = mailboxType)
+            viewModel.resetHomeScreenStates()
+        },
+        onEmailCardPressed = { email: Email ->
+            viewModel.updateDetailsScreenStates(
+                email = email
+            )
+        },
+        onDetailScreenBackPressed = {
+            viewModel.resetHomeScreenStates()
+        },
+        modifier = modifier
+    )
+}
