92a36234b7edc161992f65e8ef4a28613fbb8088
Yuhee Kim <dbgml4509@naver.com>
2022-12-16 22:57:28 +0900

    feat : 마지막 게임일 시 alert 창 내용 설정

--- Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/android/unscramble/ui/GameScreen.kt
+++ Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/android/unscramble/ui/GameScreen.kt
@@ -97,6 +97,12 @@ fun GameScreen(
                 Text(stringResource(R.string.submit))
             }
         }
+        if (gameUiState.isGameOver) {
+            FinalScoreDialog(
+                score = gameUiState.score,
+                onPlayAgain = { gameViewModel.resetGame() }
+            )
+        }
     }
 }
 
@@ -176,6 +182,7 @@ fun GameLayout(
  */
 @Composable
 private fun FinalScoreDialog(
+    score: Int,
     onPlayAgain: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -188,7 +195,7 @@ private fun FinalScoreDialog(
             // onCloseRequest.
         },
         title = { Text(stringResource(R.string.congratulations)) },
-        text = { Text(stringResource(R.string.you_scored, 0)) },
+        text = { Text(stringResource(R.string.you_scored, score))},
         modifier = modifier,
         dismissButton = {
             TextButton(
@@ -201,7 +208,9 @@ private fun FinalScoreDialog(
         },
         confirmButton = {
             TextButton(
-                onClick = onPlayAgain
+                onClick = {
+                    onPlayAgain()
+                }
             ) {
                 Text(text = stringResource(R.string.play_again))
             }
