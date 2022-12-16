a18fd6ff317577d7a58bbd73dddbde2a5bd3b3af
JuHyeokLee <bjuhyeok@gmail.com>
2022-12-17 00:22:25 +0900

    ArtSpace

--- Unit2/Pathway3/ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
+++ Unit2/Pathway3/ArtSpace/app/src/main/java/com/example/artspace/MainActivity.kt
@@ -11,10 +11,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -46,51 +43,80 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtSpace(modifier: Modifier = Modifier) {
-    val stepState by remember { mutableStateOf(1) }
+    var stepState by remember { mutableStateOf(1) }
+    var imageResource = R.drawable.fruit;
+    var title = R.string.title_1;
+    var author = R.string.artist_1;
+    var year = R.string.year_1
+    when (stepState){
+        1 -> {
+            imageResource = R.drawable.fruit;
+            title = R.string.title_1;
+            author = R.string.artist_1;
+            year = R.string.year_1;
+        }
+        2 -> {
+            imageResource = R.drawable.ship;
+            title = R.string.title_2;
+            author = R.string.artist_2;
+            year = R.string.year_2;
+        }
+        3 ->{
+            imageResource = R.drawable.island;
+            title = R.string.title_3;
+            author = R.string.artist_3;
+            year = R.string.year_3;
+        }
+    }
+
     Column(
         modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-            ,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center) {
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+            Box(modifier.weight(6f)
+            ){
             androidx.compose.material.Surface(
-
 
                         color= Color.White,
                         shape = RectangleShape,
                         elevation = 12.dp,
                         border = BorderStroke(3.dp, Color.Gray),
-                        modifier = Modifier.padding(0.dp, 36.dp )
-
+                        modifier = Modifier
+                            .padding(0.dp, 36.dp)
 
             ) {
                 Image(
-                    painter = painterResource(R.drawable.dice_1),
+                    painter = painterResource(id = imageResource),
                     contentDescription = null,
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(32.dp)
                 )
             }
+            }
             Surface(color= Color.White,
                 shape = RectangleShape,
                 elevation = 6.dp,
-                modifier = Modifier.padding(0.dp,0.dp,0.dp, 36.dp )
+                modifier = Modifier
+                    .weight(2f)
+                    .padding(0.dp, 0.dp, 0.dp, 36.dp)
 
 
             )
             {
             Column(
-                modifier.fillMaxWidth()
+                modifier
+
                     .padding(16.dp),
                 verticalArrangement = Arrangement.Center,
             ) {
-                Text(text = stringResource(R.string.sample_title),fontSize = 28.sp, fontWeight = FontWeight.Light  )
+                Text(text = stringResource(title),fontSize = 28.sp, fontWeight = FontWeight.Light  )
                 Row(
                 ) {
-                    Text(text = stringResource(R.string.sample_artist),fontWeight = FontWeight.Bold )
-                    Text(text = " (" + stringResource(R.string.sample_year) + ")")
+                    Text(text = stringResource(author),fontWeight = FontWeight.Bold )
+                    Text(text = " (" + stringResource(year) + ")")
                 }
 
             }
@@ -99,18 +125,31 @@ fun ArtSpace(modifier: Modifier = Modifier) {
                 modifier
                     .fillMaxWidth()
                     .weight(1f),
-                horizontalArrangement = Arrangement.SpaceAround
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.Bottom
             ) {
                 Button(
-                    onClick = { /*TODO*/ },
-                    modifier.weight(1f).padding(12.dp, 0.dp, 12.dp, 6.dp)
+                    onClick = {
+                              if(stepState === 1) {
+                                  stepState = 3;
+                              } else stepState -= 1
+                    },
+                    modifier
+                        .weight(1f)
+                        .padding(12.dp, 0.dp, 12.dp, 0.dp)
                 ) {
                     Text(text = "Previous", modifier.padding(2.dp))
                 }
 
                 Button(
-                    onClick = { /*TODO*/ },
-                    modifier.weight(1f).padding(12.dp, 0.dp, 12.dp, 0.dp)
+                    onClick = {
+                        if(stepState === 3) {
+                            stepState = 1;
+                        } else stepState += 1
+                    },
+                    modifier
+                        .weight(1f)
+                        .padding(12.dp, 0.dp, 12.dp, 0.dp)
                 ) {
                     Text(text = "Next" , modifier.padding(2.dp))
                 }
