180e06f4277644115b9c053a260c2645a3d5cfa6
evan <chanjungskim@gmail.com>
2022-12-17 10:59:19 +0900

    Add: affirmationscodelab

---
+++ Unit3/Pathway2/AffirmationsCodelab/app/src/main/java/com/example/affirmationscodelab/model/Affirmation.kt
@@ -0,0 +1,11 @@
+package com.example.affirmationscodelab.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes
+    val stringResourceId: Int,
+    @DrawableRes
+    val imageResourceId: Int
+)
