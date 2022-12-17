16cc330835107e6120ccd85ce935a9b991da11a9
doubleclutch30 <cometruth30@gmail.com>
2022-12-17 10:04:34 +0900

    Unit4(Pathway1) - Unscramble 게임 마지막 라운드 처리 작업

--- Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/android/unscramble/data/GameUiState.kt
+++ Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/android/unscramble/data/GameUiState.kt
@@ -4,5 +4,6 @@ data class GameUiState(
     val currentScrambledWord: String = "",
     val currentWordCount: Int = 0,
     val score: Int = 0,
-    val isGuessedWordWrong: Boolean = false
+    val isGuessedWordWrong: Boolean = false,
+    val isGameOver: Boolean = false
 )
--- Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
+++ Unit4/Pathway1/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.android.unscramble.data.GameUiState
+import com.example.android.unscramble.data.MAX_NO_OF_WORDS
 import com.example.android.unscramble.data.SCORE_INCREASE
 import com.example.android.unscramble.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -27,11 +28,11 @@ class GameViewModel : ViewModel() {
     // allWords 에서 임의의 단어 추출 후 셔플
     private fun pickRandomWordAndShuffle(): String {
         currentWord = allWords.random()
-        if (usedWords.contains(currentWord)) {
-            return pickRandomWordAndShuffle()
+        return if (usedWords.contains(currentWord)) {
+            pickRandomWordAndShuffle()
         } else {
             usedWords.add(currentWord)
-            return shuffleCurrentWord(currentWord)
+            shuffleCurrentWord(currentWord)
         }
     }
 
@@ -68,13 +69,24 @@ class GameViewModel : ViewModel() {
     }
 
     private fun updateGameState(updatedScore: Int) {
-        _uiState.update { currentState ->
-            currentState.copy(
-                isGuessedWordWrong = false,
-                currentWordCount = currentState.currentWordCount.inc(),
-                currentScrambledWord = pickRandomWordAndShuffle(),
-                score = updatedScore
-            )
+        if (usedWords.size == MAX_NO_OF_WORDS) {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    score = updatedScore,
+                    currentWordCount = currentState.currentWordCount.inc(),
+                    isGameOver = true
+                )
+            }
+        } else {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    currentWordCount = currentState.currentWordCount.inc(),
+                    currentScrambledWord = pickRandomWordAndShuffle(),
+                    score = updatedScore
+                )
+            }
         }
     }
 
