9da6614847410d73dc25edc04ddadde31dc261ef
doubleclutch30 <cometruth30@gmail.com>
2022-12-17 11:39:04 +0900

    Unit4(Pathway1) - Dessert Click 디저트 이미지 변경 및 총수익 변경

--- Unit4/Pathway1/basic-android-kotlin-compose-training-dessert-clicker/app/src/main/java/com/example/dessertclicker/ui/DessertViewModel.kt
+++ Unit4/Pathway1/basic-android-kotlin-compose-training-dessert-clicker/app/src/main/java/com/example/dessertclicker/ui/DessertViewModel.kt
@@ -10,8 +10,23 @@ import kotlinx.coroutines.flow.update
 
 class DessertViewModel : ViewModel() {
 
-    private val _uiState = MutableStateFlow(DessertUiState())
-    val uiState: StateFlow<DessertUiState> = _uiState.asStateFlow()
+    private val _dessertUiState = MutableStateFlow(DessertUiState())
+    val dessertUiState: StateFlow<DessertUiState> = _dessertUiState.asStateFlow()
+
+    // 디저트 이미지 클릭
+    fun onDessertClicked() {
+        _dessertUiState.update { state ->
+            val dessertsSold = state.dessertsSold + 1
+            val nextDessertIndex = determineDessertIndex(dessertsSold)
+            state.copy(
+                currentDessertIndex = nextDessertIndex,
+                revenue = state.revenue + state.currentDessertPrice,
+                dessertsSold = dessertsSold,
+                currentDessertImageId = dessertList[nextDessertIndex].imageId,
+                currentDessertPrice = dessertList[nextDessertIndex].price
+            )
+        }
+    }
 
     fun determineDessertIndex(dessertsSold: Int): Int {
         var dessertIndex = 0
