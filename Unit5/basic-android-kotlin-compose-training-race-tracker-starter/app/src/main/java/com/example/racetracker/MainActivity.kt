522e3899fc6b7caee5498383c7411e1fa5eb858e
devel <yesrjsdn@gmail.com>
2022-12-27 17:53:24 +0900

    Race Tracker App complete 2022-12-27

--- Unit5/basic-android-kotlin-compose-training-race-tracker-starter/app/src/main/java/com/example/racetracker/MainActivity.kt
+++ Unit5/basic-android-kotlin-compose-training-race-tracker-starter/app/src/main/java/com/example/racetracker/MainActivity.kt
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2022 The Android Open Source Project
+ * Copyright (C) 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      https://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,30 +14,29 @@
  * limitations under the License.
  */
 
-package com.example.sports
+package com.example.racetracker
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
-import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-import com.example.sports.ui.SportsApp
-import com.example.sports.ui.theme.SportsTheme
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.example.racetracker.ui.RaceTrackerApp
 
-/**
- * Activity for Sports app
- */
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            SportsTheme {
-                val windowSize = calculateWindowSizeClass(this)
-                SportsApp(
-                    windowSize = windowSize.widthSizeClass
-                )
+            MaterialTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    RaceTrackerApp()
+                }
             }
         }
     }
